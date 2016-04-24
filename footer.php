e251442dcb17ee79edeecdf3f31a07d359d769ff
ed8009 <ed8009@yandex.ru>
2016-04-24 16:46:47 +0300

    Fixed map

--- footer.php
+++ footer.php
@@ -63,7 +63,7 @@
 					<br>SANTA MONICA<br>
 					312 Arizona Avenue<br>
 					Santa Monica, CA 90401</p>
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3307.0426494208914!2d-118.49943728478584!3d34.01711618061547!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2a4cf828fe6fb%3A0x98334bfc7af2a7bf!2s312+Arizona+Ave%2C+Santa+Monica%2C+CA+90401!5e0!3m2!1sen!2sus!4v1458203894011" width="500" height="450" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3307.0426494208914!2d-118.49943728478584!3d34.01711618061547!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2a4cf828fe6fb%3A0x98334bfc7af2a7bf!2s312+Arizona+Ave%2C+Santa+Monica%2C+CA+90401!5e0!3m2!1sen!2sus!4v1458203894011" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen=""></iframe>
 				</div>
 			</div>
 
