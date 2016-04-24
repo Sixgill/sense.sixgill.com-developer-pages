91e4dcb569e0c8179cf4439b527e3b390443f323
haosx86 <haosx86@gmail.com>
2016-04-24 16:42:54 +0300

    fix diagram padding on mobiles and tablets

--- page.php
+++ page.php
@@ -47,7 +47,7 @@
 
 
 	<section id="below_products" class="container-fluid center clearfix nomargin nopadding" align="center" style="padding-bottom:0px;">
-		<img src="/wp-content/themes/sixgill/images/td/home/infographic.svg" alt="" style="padding:85px 50px 45px 50px;" />
+		<img src="/wp-content/themes/sixgill/images/td/home/infographic.svg" class="home-diagram" alt=""/>
 		<div class="clear"></div>
 		<a href="/products" class="button button-teal button-large nobottommargin btnSchedule">VIEW PRODUCTS</a>
 	</section>
