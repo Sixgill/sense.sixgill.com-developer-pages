95f573be269bc0c272a2b657b2c49c807e6a8b66
vterekhov <haosx86@gmail.com>
2016-04-28 15:33:18 +0300

    add settings files

---
+++ wp-config-local.php
@@ -0,0 +1,8 @@
+<?php
+define( 'DB_NAME', 'pantheon' );
+define( 'DB_USER', 'pantheon' );
+define( 'DB_PASSWORD', '33b66ea50d7540a7bce8450c31a35bbb' );
+define( 'DB_HOST', 'localhost' );
+define( 'WPLANG', '' );
+define( 'WP_DEBUG', true );
+define( 'SCRIPT_DEBUG', true );
