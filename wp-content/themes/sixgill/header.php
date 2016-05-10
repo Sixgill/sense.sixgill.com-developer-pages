ca75185d360f46e4d52dc3311de5acde2e57e0ec
vterekhov <haosx86@gmail.com>
2016-05-10 13:33:07 +0300

    add tablet and mobile global social buttons

--- wp-content/themes/sixgill/header.php
+++ wp-content/themes/sixgill/header.php
@@ -126,32 +126,34 @@
 				<?php } ?>
 
 				</ul>
+				<ul class="global-social-buttons-list">
+					<li>
+						<hr class="global-social-buttons-separator">
 
+						<?php
+							$globalSocialButtonsInfoId = getPageBySlug('global-social-buttons')->ID;
+
+							$facebookLink = get_field('facebook_link', $globalSocialButtonsInfoId);
+							$twitterLink = get_field('twitter_link', $globalSocialButtonsInfoId);
+							$linkedinLink = get_field('linkedin_link', $globalSocialButtonsInfoId);
+						?>
+
+						<div class="global-social-buttons-container">
+
+							<a href="<?php echo $facebookLink; ?>" class="menu-social-link">
+								<img width="13px" src="/wp-content/themes/sixgill/images/td/blog/facebook_normal.svg">
+							</a>
+							<a href="<?php echo $twitterLink; ?>" class="menu-social-link">
+								<img width="22px" src="/wp-content/themes/sixgill/images/td/blog/twitter_normal.svg">
+							</a>
+							<a href="<?php echo $linkedinLink; ?>" class="menu-social-link">
+								<img width="22px" src="/wp-content/themes/sixgill/images/td/blog/linkedin_normal.svg">
+							</a>
+
+						</div>
+					</li>
+				<ul>
 			</nav><!-- #primary-menu end -->
-
-			<hr class="global-social-buttons-separator-desktop">
-
-			<?php
-				$globalSocialButtonsInfoId = getPageBySlug('global-social-buttons')->ID;
-
-				$facebookLink = get_field('facebook_link', $globalSocialButtonsInfoId);
-				$twitterLink = get_field('twitter_link', $globalSocialButtonsInfoId);
-				$linkedinLink = get_field('linkedin_link', $globalSocialButtonsInfoId);
-			?>
-
-			<div>
-
-				<a href="<?php echo $facebookLink; ?>" class="menu-social-link">
-					<img width="13px" src="/wp-content/themes/sixgill/images/td/blog/facebook_normal.svg">
-				</a>
-				<a href="<?php echo $twitterLink; ?>" class="menu-social-link">
-					<img width="22px" src="/wp-content/themes/sixgill/images/td/blog/twitter_normal.svg">
-				</a>
-				<a href="<?php echo $linkedinLink; ?>" class="menu-social-link">
-					<img width="22px" src="/wp-content/themes/sixgill/images/td/blog/linkedin_normal.svg">
-				</a>
-
-			</div>
 
 		</div>
 
