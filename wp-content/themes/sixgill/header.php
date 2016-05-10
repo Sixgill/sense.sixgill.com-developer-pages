9b1da70d2d7ade0c679351722c5608c0cd2057ee
vterekhov <haosx86@gmail.com>
2016-05-10 16:31:41 +0300

    make social links open in a new tab

--- wp-content/themes/sixgill/header.php
+++ wp-content/themes/sixgill/header.php
@@ -140,13 +140,13 @@
 
 					<hr class="global-social-buttons-separator">
 
-					<a href="<?php echo $facebookLink; ?>" class="menu-social-link">
+					<a href="<?php echo $facebookLink; ?>" class="menu-social-link" target="_blank">
 						<img width="13px" src="/wp-content/themes/sixgill/images/td/blog/facebook_normal.svg">
 					</a>
-					<a href="<?php echo $twitterLink; ?>" class="menu-social-link">
+					<a href="<?php echo $twitterLink; ?>" class="menu-social-link" target="_blank">
 						<img width="22px" src="/wp-content/themes/sixgill/images/td/blog/twitter_normal.svg">
 					</a>
-					<a href="<?php echo $linkedinLink; ?>" class="menu-social-link">
+					<a href="<?php echo $linkedinLink; ?>" class="menu-social-link" target="_blank">
 						<img width="22px" src="/wp-content/themes/sixgill/images/td/blog/linkedin_normal.svg">
 					</a>
 
