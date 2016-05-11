936b6f589c4ec065735ca0deb523556e11ebbf53
ed8009 <ed8009@yandex.ru>
2016-05-11 18:18:52 +0300

    [SIX-135] Update button position

--- wp-content/themes/sixgill/footer.php
+++ wp-content/themes/sixgill/footer.php
@@ -18,7 +18,6 @@
 			<div class="col_half footer_Learn">Learn how Sixgill can help your business.</div>
 			<div class="col_half col_last footer_Schedule" style="border: 0px solid #FF0004;">
 				<button href="#" data-toggle="modal" data-target="#myModal" class="button button-large nobottommargin btnSchedule">SCHEDULE A DEMO</button>
-                <button href="#" data-toggle="modal" data-target="#myResource" class="button button-large nobottommargin btnSchedule">RESOURCES</button>
 			</div>
 		</div>
 	</div>
@@ -216,7 +215,7 @@ function toggle(showHideDiv, switchTextDiv) {
 
   if(document.getElementById('single-page-content')){
 
-//alert( 'Высота с учетом прокрутки: ' + window.pageYOffset);
+//alert( 'Высота с учетом прокрутки: ' + document.getElementById('footer').offsetHeight);
 
 var a = document.querySelector('#aside1'), b = null, P = 0;  // если ноль заменить на число, то блок будет прилипать до того, как верхний край окна браузера дойдёт до верхнего края элемента. Может быть отрицательным числом
 window.addEventListener('scroll', Ascroll, false);
@@ -225,6 +224,14 @@ document.body.addEventListener('scroll', Ascroll, false);
 var headerFooter = document.querySelector('footer').getBoundingClientRect().top;
 
 function Ascroll() {
+    
+
+    var heightStarted = 40;
+    var heightTop = 40;
+    
+    if (document.body.clientWidth <= 775) {
+        heightTop = 0;
+    }
     
   if (b == null) {
     var Sa = getComputedStyle(a, ''), s = '';
@@ -240,7 +247,7 @@ function Ascroll() {
     for (var i = 1; i < l; i++) {
       b.appendChild(a.childNodes[1]);
     }
-    a.style.height = b.getBoundingClientRect().height + 'px';
+    a.style.height = b.getBoundingClientRect().height + heightStarted + 'px';
     a.style.padding = '0';
     a.style.border = '0';
   }
@@ -264,14 +271,14 @@ function Ascroll() {
     else {
      P = Ra.top;
     if (((Ra.top - P) <= 0))  {
-    if (window.pageYOffset >= (scrollHeight - document.getElementById('footer').offsetHeight) ) {
+    if (window.pageYOffset >= (document.body.scrollHeight - document.getElementById('footer').offsetHeight - 500) ) {
 
       b.className = 'stop';
       b.style.top =  -R +'px';
     }
       else {
       b.className = 'sticky';
-      b.style.top = P - 211 - 40 + 'px';
+      b.style.top = P - 211 - heightTop + 'px';
 
 
     }
