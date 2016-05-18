141c5bd6fcd4d9f090db7a89f87ff6c631e03215
vterekhov <haosx86@gmail.com>
2016-05-18 14:32:37 +0300

    fix title

--- wp-content/themes/sixgill/header.php
+++ wp-content/themes/sixgill/header.php
@@ -42,17 +42,10 @@
 
 		wp_title( '|', true, 'right' );
 
-		// Add the blog name.
-		bloginfo( 'name' );
-
 		// Add the blog description for the home/front page.
 		$site_description = get_bloginfo( 'description', 'display' );
 		if ( $site_description && ( is_home() || is_front_page() ) )
 			echo " | $site_description";
-
-		// Add a page number if necessary:
-		if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() )
-			echo esc_html( ' | ' . sprintf( __( 'Page %s', 'twentyten' ), max( $paged, $page ) ) );
 
 		?></title>
 
