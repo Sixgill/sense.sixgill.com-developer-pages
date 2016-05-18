ebd0210f468c6f99c516610438be46fcb8b6a274
vterekhov <haosx86@gmail.com>
2016-05-18 12:14:52 +0300

    disable browser cache on dev and test

--- wp-content/themes/sixgill/header.php
+++ wp-content/themes/sixgill/header.php
@@ -14,7 +14,17 @@
 	<link rel="stylesheet" href="/wp-content/themes/sixgill/css/font-icons.css" type="text/css" />
 	<link rel="stylesheet" href="/wp-content/themes/sixgill/css/animate.css" type="text/css" />
 	<link rel="stylesheet" href="/wp-content/themes/sixgill/css/magnific-popup.css" type="text/css" />
-
+	<?php
+	if($_SERVER['SERVER_NAME']=="dev-sixgill.pantheonsite.io" || $_SERVER['SERVER_NAME']=="test-sixgill.pantheonsite.io") {
+	?>
+		<meta http-equiv="cache-control" content="max-age=0" />
+		<meta http-equiv="cache-control" content="no-cache" />
+		<meta http-equiv="expires" content="0" />
+		<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+		<meta http-equiv="pragma" content="no-cache" />
+	<?php
+	}
+	?>
 	<link rel="stylesheet" href="/wp-content/themes/sixgill/css/responsive.css" type="text/css" />
 
 
