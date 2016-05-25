a2eb5af8632a4a42e49f27da8c0183eb878e9971
Vladimir <haosx86@users.noreply.github.com>
2016-05-25 19:02:20 +0300

    Revert "[SIX-124] Fixed bugs in the landscape orientation"

--- wp-content/themes/sixgill/single-blog.php
+++ wp-content/themes/sixgill/single-blog.php
@@ -22,7 +22,7 @@
 
                     <?php if ( has_post_thumbnail()) { ?>
 
-                    <div class="shareIcons-desktop">
+                    <div class="shareIcons-desktop-tablet">
                        <h3 id="press-date-post" class="press-date no-bottom-margin" style="color:#000"><?php echo $post->post_title; ?></h3>
 					   <h4 id="press-author-post" class="press-by-author author-with-date-blogPost"><?php echo the_time('F j, Y'); ?> | Post by <?php echo get_field('author'); ?></h4>
 
@@ -30,18 +30,6 @@
                             <?php the_post_thumbnail( 'spec_thumb' ); ?>
 				        </div>
                         <?php include(locate_template('social_buttons.php')); ?>
-                    </div>
-                    
-                    <div class="shareIcons-tablet">
-                       <h3 id="press-date-post" class="press-date no-bottom-margin" style="color:#000; margin-bottom: 10px;"><?php echo $post->post_title; ?></h3>
-
-                        <div id="background-image-header" class="swiper-slide" style="  background-position: center; height: 350px; width:100%;     margin-bottom: 30px;">
-                            <?php the_post_thumbnail( 'spec_thumb' ); ?>
-				        </div>
-                        <?php include(locate_template('social_buttons.php')); ?>
-                        
-                        <p id="press-author-post" style="font-size: 20px; font-weight: normal; color:#000; margin-bottom: 0px; line-height: 1;"><?php echo the_time('F j, Y'); ?></p>
-                        <p id="press-author-post" style="font-size: 20px; font-weight: normal; color:#999999; line-height: 1; "> Post by <?php echo get_field('author'); ?></p>
                     </div>
 
                     <div class="shareIcons-mobile">
@@ -55,24 +43,15 @@
                     <p id="press-author-post" style="font-size: 20px; font-weight: normal; color:#000; margin-bottom: 0px; line-height: 1;"><?php echo the_time('F j, Y'); ?></p>
                     <p id="press-author-post" style="font-size: 20px; font-weight: normal; color:#999999; line-height: 1; "> Post by <?php echo get_field('author'); ?></p>
                     </div>
-                    
                     <?php } else { ?>
-                    <div class="shareIcons-desktop">
+                    <div class="shareIcons-desktop-tablet">
                         <h3 id="press-date-post" class="press-date no-bottom-margin" style="color:#000"><?php echo $post->post_title; ?></h3>
                         
                         <?php include(locate_template('social_buttons.php')); ?>
                         <h4 id="press-author-post" class="press-by-author author-with-date-blogPost"><?php echo the_time('F j, Y'); ?> | Post by <?php echo get_field('author'); ?></h4>
                     </div>
-                    
-                    <div class="shareIcons-tablet">
-                       <h3 id="press-date-post" class="press-date no-bottom-margin" style="color:#000"><?php echo $post->post_title; ?></h3>
-					   <h4 id="press-author-post" class="press-by-author author-with-date-blogPost"><?php echo the_time('F j, Y'); ?> | Post by <?php echo get_field('author'); ?></h4>
 
-
-                        <?php include(locate_template('social_buttons.php')); ?>
-                    </div>
-
-                    <div class="shareIcons-mobile" >
+                    <div class="shareIcons-mobile">
                          <h3 id="press-date-post" class="press-date no-bottom-margin" style="color:#000; margin-bottom: 30px;"><?php echo $post->post_title; ?></h3>
                         
                         <?php include(locate_template('social_buttons.php')); ?>
--- wp-content/themes/sixgill/single-press.php
+++ wp-content/themes/sixgill/single-press.php
@@ -20,58 +20,33 @@
 				<div id="press-text-post" class="col_full press-post">
 
 					<?php if ( has_post_thumbnail()) { ?>
+						<div class="shareIcons-desktop-tablet">
+							<h3 id="press-date-post" class="press-date no-bottom-margin" style="color:#000"><?php echo $post->post_title; ?></h3>
+							<h4 id="press-author-post" class="press-by-author author-with-date-blogPost"><?php echo the_time('F j, Y'); ?> | Post by <?php echo get_field('author'); ?></h4>
 
-                    <div class="shareIcons-desktop">
-                       <h3 id="press-date-post" class="press-date no-bottom-margin" style="color:#000"><?php echo $post->post_title; ?></h3>
-					   <h4 id="press-author-post" class="press-by-author author-with-date-blogPost"><?php echo the_time('F j, Y'); ?> | Post by <?php echo get_field('author'); ?></h4>
+							<div id="background-image-header" class="swiper-slide" style="  background-position: center; height: 350px; width:100%;     margin-bottom: 30px;">
+								<?php the_post_thumbnail( 'spec_thumb' ); ?>
+							</div>
+                                <?php include(locate_template('social_buttons.php')); ?>
+						</div>
 
-                        <div id="background-image-header" class="swiper-slide" style="  background-position: center; height: 350px; width:100%;     margin-bottom: 30px;">
-                            <?php the_post_thumbnail( 'spec_thumb' ); ?>
-				        </div>
-                        <?php include(locate_template('social_buttons.php')); ?>
-                    </div>
-                    
-                    <div class="shareIcons-tablet">
-                       <h3 id="press-date-post" class="press-date no-bottom-margin" style="color:#000; margin-bottom: 10px;"><?php echo $post->post_title; ?></h3>
-
-                        <div id="background-image-header" class="swiper-slide" style="  background-position: center; height: 350px; width:100%;     margin-bottom: 30px;">
-                            <?php the_post_thumbnail( 'spec_thumb' ); ?>
-				        </div>
-                        <?php include(locate_template('social_buttons.php')); ?>
-                        
-                        <p id="press-author-post" style="font-size: 20px; font-weight: normal; color:#000; margin-bottom: 0px; line-height: 1;"><?php echo the_time('F j, Y'); ?></p>
-                        <p id="press-author-post" style="font-size: 20px; font-weight: normal; color:#999999; line-height: 1; "> Post by <?php echo get_field('author'); ?></p>
-                    </div>
-
-                    <div class="shareIcons-mobile">
-                       <h3 id="press-date-post" class="press-date no-bottom-margin" style="color:#000; margin-bottom: 10px;"><?php echo $post->post_title; ?></h3>
-                        <div id="background-image-header" class="swiper-slide" style="  background-position: center; height: 350px; width:100%;     margin-bottom: 30px;">
-                            <?php the_post_thumbnail( 'spec_thumb' ); ?>
-				        </div>
-                        
-                        <?php include(locate_template('social_buttons.php')); ?>
-
-                    <p id="press-author-post" style="font-size: 20px; font-weight: normal; color:#000; margin-bottom: 0px; line-height: 1;"><?php echo the_time('F j, Y'); ?></p>
-                    <p id="press-author-post" style="font-size: 20px; font-weight: normal; color:#999999; line-height: 1; "> Post by <?php echo get_field('author'); ?></p>
-                    </div>
-                    
-                    <?php } else { ?>
-							<div class="shareIcons-desktop">
+						<div class="shareIcons-mobile">
+							<h3 id="press-date-post" class="press-date no-bottom-margin" style="color:#000; margin-bottom: 10px;"><?php echo $post->post_title; ?></h3>
+							<div id="background-image-header" class="swiper-slide" style="  background-position: center; height: 350px; width:100%;     margin-bottom: 30px;">
+								<?php the_post_thumbnail( 'spec_thumb' ); ?>
+							</div>
+                            
+                                <?php include(locate_template('social_buttons.php')); ?>
+							<p id="press-author-post" style="font-size: 20px; font-weight: normal; color:#000; margin-bottom: 0px; line-height: 1;"><?php echo the_time('F j, Y'); ?></p>
+							<p id="press-author-post" style="font-size: 20px; font-weight: normal; color:#999999; line-height: 1; "> Post by <?php echo get_field('author'); ?></p>
+						</div>
+						<?php } else { ?>
+							<div class="shareIcons-desktop-tablet">
 								<h3 id="press-date-post" class="press-date no-bottom-margin" style="color:#000"><?php echo $post->post_title; ?></h3>
                                 <?php include(locate_template('social_buttons.php')); ?>
 
 								<h4 id="press-author-post" class="press-by-author author-with-date-blogPost"><?php echo the_time('F j, Y'); ?> | Post by <?php echo get_field('author'); ?></h4>
 							</div>
-
-                            <div class="shareIcons-tablet">
-                                <h3 id="press-date-post" class="press-date no-bottom-margin" style="color:#000; margin-bottom: 10px;"><?php echo $post->post_title; ?></h3>
-
-      
-                                <?php include(locate_template('social_buttons.php')); ?>
-                        
-                                <p id="press-author-post" style="font-size: 20px; font-weight: normal; color:#000; margin-bottom: 0px; line-height: 1;"><?php echo the_time('F j, Y'); ?></p>
-                                <p id="press-author-post" style="font-size: 20px; font-weight: normal; color:#999999; line-height: 1; "> Post by <?php echo get_field('author'); ?></p>
-                            </div>
 
 							<div class="shareIcons-mobile">
 								<h3 id="press-date-post" class="press-date no-bottom-margin" style="color:#000; margin-bottom: 30px;"><?php echo $post->post_title; ?></h3>
