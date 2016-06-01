0128a33a87591116de17229fcaf9f61ddf7141c6
ed8009 <ed8009@yandex.ru>
2016-06-01 15:35:17 +0300

    [SIX-151] I did the code refactoring

--- wp-content/themes/sixgill/js/custom/search-header.js
+++ wp-content/themes/sixgill/js/custom/search-header.js
@@ -1,61 +1,61 @@
 jQuery(function($) {
 	var searchOpenned = false;
-  //Chache DOM search
-  var searchQueryInput = $('#search-query');
-  var searchButton = $('#search-button');
-  var searchForm = $('#search-form');
+	//Chache DOM search
+	var searchQueryInput = $('#search-query');
+	var searchButton = $('#search-button');
+	var searchForm = $('#search-form');
 
-  function hideSearch() {
-  	searchOpenned = false;
-  	searchButton.addClass('closed');
-    searchQueryInput.addClass('closed');
-  }
+	function hideSearch() {
+		searchOpenned = false;
+		searchButton.addClass('closed');
+		searchQueryInput.addClass('closed');
+	}
   
-  function showSearch() {
-  	searchOpenned = true;
-    searchButton.removeClass('closed');
-    searchQueryInput.removeClass('closed');
-    searchQueryInput.focus();
-  }
+	function showSearch() {
+		searchOpenned = true;
+		searchButton.removeClass('closed');
+		searchQueryInput.removeClass('closed');
+		searchQueryInput.focus();
+	}
   
-  function validateSearchForm(searchQuery) {
-    //TODO: improve validation
-    //Validation passed: true
-    //Otherwise: false
-    var validationResult = true;
-    if(searchQuery.length==0) {
-    	validationResult = false;
+	function validateSearchForm(searchQuery) {
+		//TODO: improve validation
+		//Validation passed: true
+		//Otherwise: false
+		var validationResult = true;
+		if(searchQuery.length==0) {
+			validationResult = false;
+		}
+		//Add here more conditions with validationResult = false action
+		return validationResult;
     }
-    //Add here more conditions with validationResult = false action
-    return validationResult;
-  }
   
-  searchForm.submit(function(event) {
-  	if(!validateSearchForm(searchQueryInput.val())) {
-    	alert("Search query is not valid");
-      event.preventDefault();
-    }
-  });
+	searchForm.submit(function(event) {
+		if(!validateSearchForm(searchQueryInput.val())) {
+			alert("Search query is not valid");
+			event.preventDefault();
+		}
+	});
   
 	searchButton.click(function() {
-    if(searchOpenned) {
-    	if(searchQueryInput.val().length>0) {
-      	searchForm.submit();
-      } else {
-      	hideSearch();
-      }
-    } else {
-    	showSearch();
-    }
-  });
+		if(searchOpenned) {
+			if(searchQueryInput.val().length>0) {
+				searchForm.submit();
+			} else {
+				hideSearch();
+			}
+		} else {
+			showSearch();
+		}
+	});
   
-  searchForm.focusout(function() {
-  	if(searchQueryInput.val().length==0) {
-    	setTimeout(function() {
-      	if(searchOpenned) {
-        	hideSearch();
-        }
-      }, 100);
-    }
-  });
+	searchForm.focusout(function() {
+		if(searchQueryInput.val().length==0) {
+			setTimeout(function() {
+				if(searchOpenned) {
+					hideSearch();
+				}
+			}, 100);
+		}
+	});
 });
