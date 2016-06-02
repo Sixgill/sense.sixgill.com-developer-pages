776f20034671abbada339e0dae397ce005cc3597
ed8009 <ed8009@yandex.ru>
2016-06-02 12:07:42 +0300

    [SIX-151] I did the code refactoring

--- wp-content/themes/sixgill/searchform.php
+++ wp-content/themes/sixgill/searchform.php
@@ -1,4 +1,4 @@
-<form id="searchform"  style="margin-top:5px" class="form-search" role="search" method="get"  action="<?php echo home_url('/'); ?>">
+<form id="searchform" style="margin-top:5px" class="form-search" role="search" method="get" action="<?php echo home_url('/'); ?>">
 	<div class="search-page">
 		<input type="search" class="form-control" placeholder="Search" value="<?php echo get_search_query() ?>" name="s" title="Search">
 		<button type="submit" value=""></button>
