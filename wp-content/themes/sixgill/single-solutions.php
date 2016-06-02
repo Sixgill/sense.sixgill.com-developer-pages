59c581da93f3ad9e653a21a392c783b88ee22891
vterekhov <haosx86@gmail.com>
2016-06-02 12:58:25 +0300

    refactoring

--- wp-content/themes/sixgill/single-solutions.php
+++ wp-content/themes/sixgill/single-solutions.php
@@ -1,5 +1,5 @@
 <a name="top"></a>
-<section id="slider" class="slider-parallax no-border swiper_wrapper clearfix">
+<section id="slider" class="slider-parallax no-border swiper_wrapper clearfix section-margin-fix">
 	<div class="slider-parallax-inner">
 		<div class="swiper-container swiper-parent">
 			<div class="swiper-wrapper">
@@ -19,24 +19,22 @@
 </section>
 
 <!-- Content-->
-<section id="content">
-  <div id="single-post-content" class="content-wrap" style="padding-bottom:0px;">
-    <section class="header-stick">
-	    <div class="container-fluid clearfix center solution-subpage-container">
-				<div class="left-aligned subsolution-left-aligned">
-					<div class="solutions-subpage-text <?php if(!has_post_thumbnail()) echo "solution-subpage-onlytext" ?>">
-						<?php the_content(); ?>
-					</div>
-					<div class="solutions-subpage-thumbnail">
-						<?php the_post_thumbnail(); ?>
-					</div>
+<section id="content" class="section-margin-fix">
+  <div id="single-post-content" class="content-wrap no-bottom-padding">
+    <div class="container-fluid clearfix center solution-subpage-container">
+			<div class="left-aligned subsolution-left-aligned">
+				<div class="solutions-subpage-text <?php if(!has_post_thumbnail()) echo "solution-subpage-onlytext" ?>">
+					<?php the_content(); ?>
 				</div>
-	    </div>
-			<div class="col_full center">
-				<a href="/solutions/" id="solutions-button-back" class="button button-large no-bottom-margin btnCompany">
-					BACK TO SOLUTIONS
-				</a>
+				<div class="solutions-subpage-thumbnail">
+					<?php the_post_thumbnail(); ?>
+				</div>
 			</div>
-    </section>
+    </div>
+		<div class="col_full center">
+			<a href="/solutions/" id="solutions-button-back" class="button button-large no-bottom-margin btnCompany">
+				BACK TO SOLUTIONS
+			</a>
+		</div>
   </div>
 </section><!-- #content end -->
