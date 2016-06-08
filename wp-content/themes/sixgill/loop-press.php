c09a6674759b3bc86b9f7a91cd70e9c6cb3b173c
ed8009 <ed8009@yandex.ru>
2016-06-08 18:34:58 +0300

    [SIX-237] Edit Page Press in accordance with mocap

--- wp-content/themes/sixgill/loop-press.php
+++ wp-content/themes/sixgill/loop-press.php
@@ -12,12 +12,15 @@ if ( $query->have_posts() ) {
 		$query->the_post();
 ?>
 		<div class="posts-list">
-			<p id="post-subtitle-and-author" class="blog-post-suptitle press-post-suptitle color-999999">
+			<p id="post-subtitle-and-author" class="blog-post-suptitle press-post-suptitle color-999999 display-only-desktop">
 				<?php the_time('F j, Y'); ?>
 			</p>
 			<a href="<?php echo get_permalink(); ?>" id="post-title-page-blog" class="press-post-title" >
 				<?php the_title(); ?>
 			</a>
+			<p id="post-subtitle-and-author" class="blog-post-suptitle press-post-suptitle color-999999 no-desktop-display">
+				<?php the_time('F j, Y'); ?>
+			</p>
 			<?php
 				the_content("read more");
 			?>
