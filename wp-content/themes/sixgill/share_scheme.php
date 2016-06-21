bfb5ffdb9800490d9003ca533d7d34aad27e1e40
ed8009 <ed8009@yandex.ru>
2016-06-21 12:19:58 +0300

    [SIX-112] Add conditions to the meta tags

--- wp-content/themes/sixgill/share_scheme.php
+++ wp-content/themes/sixgill/share_scheme.php
@@ -1,13 +1,19 @@
-  <meta property="og:type" content="website" /> 
-  <meta property="og:title schema:name" content="<?php echo $post->post_title; ?>" />
-  <link property="og:image schema:image" href="<?php echo get_field('post_picture_to_share'); ?>" />
-  <link property="og:url schema:url" href="<?php echo wp_get_shortlink(); ?>" />
-  <meta property="og:description schema:description" content="<?php  
-  if (strlen(get_field('descriptions_post_to_share')) == 0)  {
-  	echo the_content("read more");
-  }
-  else {
-  	echo get_field('descriptions_post_to_share');
-  }
-?>" />
-  <meta property="og:site_name" content="<?php the_content(); ?>"/>
+ 	<meta property="og:type" content="website" /> 
+ 	<meta property="og:title schema:name" content="<?php echo $post->post_title; ?>" />
+
+ 	<link property="og:image schema:image" href="<?php 
+ 		if (!has_post_thumbnail()) { 
+ 			echo get_field('post_picture_to_share'); 
+		} ?>" 
+	/>
+
+ 	<link property="og:url schema:url" href="<?php echo wp_get_shortlink(); ?>" />
+
+ 	<meta property="og:description schema:description" content="<?php  
+		if (strlen(get_field('descriptions_post_to_share')) == 0)  {
+ 			echo substr($post->post_content, 0, 80);
+		}
+		else {
+			echo get_field('descriptions_post_to_share');
+		} ?>" 
+	/>
