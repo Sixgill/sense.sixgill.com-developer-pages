1b576dc54b8b48ec219c392570a2d2013559018b
vterekhov <haosx86@gmail.com>
2016-06-22 16:19:05 +0300

    move styles to css

--- wp-content/themes/sixgill/global_social_buttons.php
+++ wp-content/themes/sixgill/global_social_buttons.php
@@ -10,15 +10,15 @@
 	<hr class="global-social-buttons-separator">
 	<div>
 		<a href="<?php echo $facebookLink; ?>" class="menu-social-link" target="_blank">
-			<img alt="Share in Facebook" width="13px" src="/wp-content/themes/sixgill/images/td/blog/facebook_normal.svg">
+			<img alt="Share in Facebook" class="global-facebook-icon" src="/wp-content/themes/sixgill/images/td/blog/facebook_normal.svg">
 		</a>
 
 		<a href="<?php echo $twitterLink; ?>" class="menu-social-link" target="_blank">
-			<img alt="Share in Twitter" width="22px" src="/wp-content/themes/sixgill/images/td/blog/twitter_normal.svg">
+			<img alt="Share in Twitter" class="global-twitter-icon" src="/wp-content/themes/sixgill/images/td/blog/twitter_normal.svg">
 		</a>
 
 		<a href="<?php echo $linkedinLink; ?>" class="menu-social-link" target="_blank">
-			<img alt="Share in Linkedin" width="22px" src="/wp-content/themes/sixgill/images/td/blog/linkedin_normal.svg">
+			<img alt="Share in Linkedin" class="global-linkedin-icon" src="/wp-content/themes/sixgill/images/td/blog/linkedin_normal.svg">
 		</a>
 	</div>
 </div>
