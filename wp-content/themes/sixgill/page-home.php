24a20d15ab11056396f6ee0a1ed1c1ae504e6812
vterekhov <haosx86@gmail.com>
2016-06-24 17:57:54 +0300

    in progress

--- wp-content/themes/sixgill/page-home.php
+++ wp-content/themes/sixgill/page-home.php
@@ -131,86 +131,26 @@
 
 	<a name="clients"></a>
 	<section id="clients" class="color-white center section-margin-fix">
-		<div class="center">
+		<div class="center home-clients-block">
+
 			<div class="fancy-title-without-bottom fancy-title title-dotted-border title-center mobile-hiden-line">
 				<h3 id="home-text-company-with-line">
-					<?php echo get_field('logo_header_text'); ?>
+					<?php echo get_field('logos_title'); ?>
 				</h3>
 			</div>
-      <p class="logo-subheader">
-				<?php echo get_field('logo_header_subheader'); ?>
-			</p>
-			<div class="home-diagram hidden-top-imageDiagram max-width-720 no-mobile-portrait-display">
-				<?php foreach(get_field('logo_images_part1') as $logo) : ?>
-					<div class="col_one_fifth">
-						<img src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>" />
-					</div>
-				<?php endforeach; ?>
-				<div class="col_one_fifth col_last">
-					<img src="<?php echo get_field('last_image_logos1'); ?>" alt=""/>
-				</div>
 
-				<?php foreach(get_field('logo_images_part2') as $logo) : ?>
-					<div class="col_one_fifth">
-						<img src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>" />
-					</div>
-				<?php endforeach; ?>
-				<div class="col_one_fifth col_last">
-					<img src="<?php echo get_field('last_image_logos2'); ?>" alt=""/>
-				</div>
-				<a href="/company/" class="button button-large no-bottom-margin btnCompany">
-					<div class="vertical-centered">
-						VIEW COMPANY
-					</div>
-				</a>
+			<p class="logo-subheader">
+				<?php echo get_field('logos_subtitle'); ?>
+			</p>
+
+			<div class="home-logos">
+				<?php foreach(get_field('logos') as $logo) : ?><div class="home-logo-container center">
+						<div class="vertical-centered">
+							<img src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>" />
+						</div>
+					</div><?php endforeach; ?>
 			</div>
-			<div class="home-diagram hidden-bottom-imageDiagram no-desktop-display no-tablet-display no-mobile-landscape-display">
-				<div>
-					<div class="col-48-left">
-						<img id="home-mobile-image-att" src="/wp-content/themes/sixgill/images/td/home/logos/logo_att.jpg" alt="" />
-					</div>
-					<div class="col-48-right">
-						<img id="home-mobile-image-rugby" src="/wp-content/themes/sixgill/images/td/home/logos/logo_rugby.jpg" alt="" />
-					</div>
-				</div>
-				<div>
-					<div class="col-48-left">
-						<img id="home-mobile-image-nyautoshow" src="/wp-content/themes/sixgill/images/td/home/logos/logo_nyautoshow.jpg" alt="" />
-					</div>
-					<div class="col-48-right">
-						<img id="home-mobile-image-nyfashion" src="/wp-content/themes/sixgill/images/td/home/logos/logo_nyfashion.jpg" alt="" />
-					</div>
-				</div>
-				<div>
-					<div  class="col-48-left">
-						<img id="home-mobile-image-livenation" src="/wp-content/themes/sixgill/images/td/home/logos/logo_livenation.jpg" alt="" />
-					</div>
-					<div  class="col-48-right">
-						<img id="home-mobile-image-lemon" src="/wp-content/themes/sixgill/images/td/home/logos/logo_lemon.jpg" alt="" />
-					</div>
-				</div>
-				<div>
-					<div class="col-48-left">
-						<img id="home-mobile-image-img" src="/wp-content/themes/sixgill/images/td/home/logos/logo_img.jpg" alt="" />
-					</div>
-					<div  class="col-48-right">
-						<img id="home-mobile-image-hsbc" src="/wp-content/themes/sixgill/images/td/home/logos/logo_hsbc.jpg" alt="" />
-					</div>
-				</div>
-				<div class="home-content-padding">
-					<div class="col-48-left ">
-						<img id="home-mobile-image-auburn" src="/wp-content/themes/sixgill/images/td/home/logos/logo_auburn.jpg" alt="" />
-					</div>
-					<div class="col-48-right">
-						<img id="home-mobile-image-nyjets" src="/wp-content/themes/sixgill/images/td/home/logos/logo_nyjets.jpg" alt="" />
-					</div>
-				</div>
-			</div>
-			<div class="home-diagram hidden-bottom-imageDiagram max-width-720 no-horizontal-padding no-desktop-display no-tablet-display no-mobile-landscape-display">
-				<a href="/company/" id="home-button-image-company" class="button button-large no-bottom-margin btnCompany">
-					VIEW COMPANY
-				</a>
-			</div>
+
 		</div>
 	</section>
 </section><!-- #content end -->
