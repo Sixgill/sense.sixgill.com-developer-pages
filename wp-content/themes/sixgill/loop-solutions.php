f3198874b7de8b9188b1bbd8d9047d3fd249980a
ed8009 <ed8009@yandex.ru>
2016-06-27 14:21:49 +0300

    [SIX-290][SIX-321] Fix a bug with the appearance of the subtitles

--- wp-content/themes/sixgill/loop-solutions.php
+++ wp-content/themes/sixgill/loop-solutions.php
@@ -1,8 +1,16 @@
 <?php
 $subpages = getChindrenByParentSlug("solutions");
+$flagPrint = false;
+
 foreach($subpages as $subpage) {
 	if(get_field('page_part', $subpage->ID) == $categoryName ) {
-		?>
+
+		if (!$flagPrint) {
+			$flagPrint = true; ?>
+		<b style="padding-bottom: 5px;"> 
+			<?php echo get_field($supTitleCategory); ?>
+		</b>
+		<?php } ?>
 		<div class="solutions-subpages-list">
 			<a href="<?php echo $subpage->guid; ?>">
 				<?php echo $subpage->post_title; ?>
