65b7bd0a7b716042daded6971303134bc0c97846
ed8009 <ed8009@yandex.ru>
2016-06-28 21:16:12 +0300

    [SIX-275] Added styles for tablets and mobile phones

--- wp-content/themes/sixgill/single-solutions.php
+++ wp-content/themes/sixgill/single-solutions.php
@@ -4,7 +4,7 @@
 			<div id="post-title-slider" class="title-slider-subpage">
 				<?php echo $post->post_title; ?>
 			</div>
-			<dev id="post-subtitle-slider" class="single-post-subtitle font-size-45">
+			<dev id="post-subtitle-slider" class="single-post-subtitle font-size-title-subtitle">
 				<?php echo get_field('subtitle'); ?>
 			</dev>
 		</div>
@@ -14,26 +14,38 @@
 <div id="first-block-with-image" class="first-block-images-subpage">
 	<div id="first-left-container" class="first-left-container-subpage fleft full-height-container-subpage">
 		<div class="first-text-subpage">
-			<h1 class="font-size-30"><?php echo get_field('one_title_subpage'); ?></h1>
+			<h1 class="font-size-title-first-text"><?php echo get_field('one_title_subpage'); ?></h1>
 			<p class="color-2e2e2e"><?php echo get_field('one_text_subpage'); ?></p>
 		</div>
 	</div>
 
 	<div id="first-right-container" class="first-right-container-subpage fleft full-height-container-subpage">
-		<img src="<?php echo get_field('one_image_subpage'); ?>" alt=""/>
+		<img src="<?php echo get_field('one_image_subpage'); ?>" alt="" class="image-subpage"/>
 	</div>
 </div>
 
-<div id="second-block-with-image" class="second-block-images-subpage">
+<div id="second-block-with-image" class="second-block-images-subpage no-mobile-portrait-display no-mobile-landscape-display">
 	<div id="second-left-container" class="center second-left-container-subpage fleft full-height-container-subpage">
 		<img src="<?php echo get_field('second_image_subpage'); ?>" alt="" class="full-height-container-subpage"/>
 	</div>
 
 	<div id="second-right-container" class="second-right-container-subpage fleft full-height-container-subpage">
 		<div class="second-text-subpage">
-			<h1 class="font-size-30"><?php echo get_field('second_title_subpage'); ?></h1>
+			<h1 class="font-size-title-first-text"><?php echo get_field('second_title_subpage'); ?></h1>
 			<p><?php echo get_field('second_text_subpage'); ?></p>
 		</div>
+	</div>
+</div>
+
+<div id="second-block-with-image" class="second-block-images-subpage no-desktop-display no-tablet-display">
+	<div id="second-right-container" class="second-right-container-subpage fleft full-height-container-subpage">
+		<div class="second-text-subpage">
+			<h1 class="font-size-title-first-text"><?php echo get_field('second_title_subpage'); ?></h1>
+			<p><?php echo get_field('second_text_subpage'); ?></p>
+		</div>
+	</div>
+	<div id="second-left-container" class="center second-left-container-subpage fleft full-height-container-subpage">
+		<img src="<?php echo get_field('second_image_subpage'); ?>" alt="" class="full-height-container-subpage"/>
 	</div>
 </div>
 
@@ -54,7 +66,6 @@
 </div>
 
 <!-- Content-->
-<section id="content">
 	<div id="single-post-content" class="content-wrap no-bottom-padding no-padding-top">
 		<div class="container-subpage clearfix centerno-margin-top">
 			<div class="topmargin-lg">
@@ -62,5 +73,4 @@
 			</div>
 		</div>
   </div>
-</section>
 <!-- #content end -->
