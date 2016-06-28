1a0e5ef43917207cadbd3551f0eb76c0c9017fe5
vterekhov <haosx86@gmail.com>
2016-06-28 16:10:04 +0300

    change minifier settings

--- wp-content/plugins/bwp-minify/min/config.php
+++ wp-content/plugins/bwp-minify/min/config.php
@@ -3,7 +3,7 @@ $min_enableBuilder = false;
 $min_builderPassword = 'admin';
 $min_errorLogger = false;
 $min_allowDebugFlag = false;
-$min_cachePath = dirname(dirname(__FILE__)) . '/cache';
+$min_cachePath = dirname(dirname(__FILE__)) . '../../uploads';
 $min_documentRoot = '';
 $min_cacheFileLocking = true;
 $min_serveOptions['bubbleCssImports'] = false;
@@ -13,4 +13,4 @@ $min_symlinks = array();
 $min_uploaderHoursBehind = 0;
 $min_libPath = dirname(__FILE__) . '/lib';
 ini_set('zlib.output_compression', '0');
-// auto-generated on 2016-06-06 21:52:42
+// auto-generated on 2016-06-06 22:40:55
