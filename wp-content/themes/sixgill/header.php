ea5c571731db7ef403fd9eae1cf33483915477a4
vterekhov <haosx86@gmail.com>
2016-07-18 16:20:46 +0300

    fix desktop menu border

--- wp-content/themes/sixgill/header.php
+++ wp-content/themes/sixgill/header.php
@@ -50,7 +50,7 @@
 		<section id="top"></section>
 		<!-- Header -->
 		<div id="menu-background"></div>
-		<header id="header" class="no-sticky no-border">
+		<header id="header" class="no-sticky">
 			<div id="header-wrap">
 				<div id="logo" class="noborder nopadding">
 					<a href="/" class="logo-sixgill"></a>
