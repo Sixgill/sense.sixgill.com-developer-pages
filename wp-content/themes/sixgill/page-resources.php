97b021415c4e9a1899e622c18baea104b3c0fbe5
vterekhov <haosx86@gmail.com>
2016-07-27 18:18:54 +0300

    remove fullpage from products

--- wp-content/themes/sixgill/page-resources.php
+++ wp-content/themes/sixgill/page-resources.php
@@ -1,29 +1,30 @@
 <?php get_header(); ?>
-<iframe id="frame1" class="hidden"></iframe>
-<a name="top"></a>
-<section id="slider" class="slider-parallax swiper_wrapper swiper_wrapper_small clearfix">
-	<div class="slider-parallax-inner">
-		<div class="swiper-container swiper-parent">
-			<div class="swiper-wrapper">
-				<div class="swiper-slide resources-page-background">
-					<div class="slider-caption slider-caption-center">
-						<h1 class="single-post-title">
-							RESOURCES
-						</h1>
+<div> <!-- fullpage wrapper placeholder -->
+	<iframe id="frame1" class="hidden"></iframe>
+	<a name="top"></a>
+	<section id="slider" class="slider-parallax swiper_wrapper swiper_wrapper_small clearfix">
+		<div class="slider-parallax-inner">
+			<div class="swiper-container swiper-parent">
+				<div class="swiper-wrapper">
+					<div class="swiper-slide resources-page-background">
+						<div class="slider-caption slider-caption-center">
+							<h1 class="single-post-title">
+								RESOURCES
+							</h1>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</section><!-- Content-->
+	</section><!-- Content-->
 
-<section id="content" class="section-margin-fix">
+	<section id="content" class="section-margin-fix">
 
-	<div class="content-wrap resources-container resources-background">
-		<div class="resources-blocks-container">
-			<?php echo do_shortcode('[sdm_show_dl_from_category category_slug="resources" fancy="2"]'); ?>
+		<div class="content-wrap resources-container resources-background">
+			<div class="resources-blocks-container">
+				<?php echo do_shortcode('[sdm_show_dl_from_category category_slug="resources" fancy="2"]'); ?>
+			</div>
 		</div>
-	</div>
 
-</section><!-- #content end -->
-<?php get_footer(); ?>
+	</section><!-- #content end -->
+	<?php get_footer(); ?>
