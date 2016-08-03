d83e684a6a305ac331d33b916bc467a4dabf2265
Ilya Napastyuk <napastyuk@gmail.com>
2016-08-03 12:52:48 +0300

    home page redesign

--- wp-content/themes/sixgill/page-home.php
+++ wp-content/themes/sixgill/page-home.php
@@ -7,8 +7,8 @@
 					<h1 id="home-text-on-video" data-animate="fadeInDown" class="txtVideo home-video-text-h1">
 					<?php echo get_field('blue_header_text_below_video'); ?>
 					</h1>
-					<button id="home-button-on-video" data-animate="fadeInDown" data-toggle="modal" data-target="#myModal" class="button button-large no-bottom-margin btnSchedule">
-					SCHEDULE A DEMO
+					<button id="home-button-on-video" data-animate="fadeInDown" data-toggle="modal" data-target="#myModal" class="button button-green no-bottom-margin btnSchedule">
+					Schedule a Demo
 					</button>
 				</div>
 			</div>
@@ -26,110 +26,119 @@
 <!-- Content-->
 <a name="what"></a>
 <section id="content">
-	<section id="what" class="container-fluid center clearfix no-margin nopadding section-margin-fix home-page-section fp-auto-height">
-		<section id="what2" class="container-fluid center clearfix">
-			<div class="home-text-2 center">
-				<div id="home-subtitle-with-line" class="homeSupTit">
-					<span class="homeSupTit-with-line">
-						<?php echo get_field('the_text_on_the_line'); ?>
-					</span>
-				</div>
-				<hr class="hr-home-sup-title" />
-				<div id="home-block-what2-title" class="homeTit">
-					<?php echo get_field('large_text_title'); ?>
-				</div>
-				<div id="home-block-what2-subtitle" class="homeSubTit">
-					<?php echo get_field('the_text_below_the_title'); ?>
-				</div>
-				<hr class="hr-home-sub-title" />
-				<div id="home-main-text">
-					<?php echo get_field('main_text'); ?>
-				</div>
+	<section id="what" class="container-fluid center clearfix no-margin nopadding section-margin-fix full-screen-height home-page-section fp-auto-height">
+		<div class="home-block-what-title-wrapper">
+			<div class="home-block-what-subtitle-on-line">
+				<?php echo get_field('the_text_on_the_line'); ?>
 			</div>
-		</section>
-		<section id="after_what2" class="container-fluid center clearfix no-margin nopadding section-margin-fix">
-			<img id="home-image-background-after_what2" src="/wp-content/themes/sixgill/images/td/home/00_FocusSection_v2.jpg" class="full-width" alt=""/>
-		</section>
-	</section>
-	<a name="products"></a>
-	<section id="products" class="container-fluid center clearfix no-margin nopadding section-margin-fix home-page-section fp-auto-height">
-		<div id="home-text-productions" class="home-products-title"><?php echo get_field('text_productions'); ?></div>
-	</section>
-	<section id="below_products" class="container-fluid center clearfix no-margin nopadding section-margin-fix home-page-section fp-auto-height">
-		<img id="home-image-infographic" src="/wp-content/themes/sixgill/images/td/home/infographic.svg" class="home-diagram display-only-desktop" alt=""/>
-		<img id="home-image-mobile-infographic" src="/wp-content/themes/sixgill/images/td/home/mobile_infographic.svg" class="home-diagram hidden-bottom-imageDiagram no-desktop-display" alt=""/>
-		<div class="clear"></div>
-		<a href="/products/" id="home-button-infographic" class="button button-teal button-large no-bottom-margin btnSchedule">
-			VIEW PRODUCTS
-		</a>
-	</section>
-	<section id="after_products" class="container-fluid center clearfix no-margin nopadding section-margin-fix home-page-section fp-auto-height">
-		<img id="home-background-after-products" src="/wp-content/themes/sixgill/images/td/home/00_IconsSection_Upper.jpg" class="full-width" alt=""/>
-	</section>
-	<a name="solutions"></a>
-	<section id="solutions" class="container-fluid background-2e2e2e center clearfix no-margin nopadding section-margin-fix home-page-section fp-auto-height" >
-		<div>
-			<div class="container clearfix">
-				<div class="col_one_third">
-					<img id="home-image-proximity-people" src="<?php echo get_field('image_people_home'); ?>" alt=""/>
-					<br>
-					<div id="home-title-proximity-people" class="home3Tit">
-						<?php echo get_field('title_people'); ?>
-					</div>
-					<div id="home-text-proximity-people" class="home3Txt">
-						<?php echo get_field('text_people'); ?>
-					</div>
+			<hr class="hr-home-suptitle-line" />
+			<div class="home-block-what-title">
+				<?php echo get_field('large_text_title'); ?>
+			</div>
+			<div class="home-block-what-subtitle">
+				<?php echo get_field('the_text_below_the_title'); ?>
+			</div>
+			<hr class="hr-home-suptitle-line" />
+		</div>
+		<div class="home-block-what-wrapper">
+			<div class="home-block-what-col">
+				<div class="home-block-what-item">
+					<img class="home-block-what-item-icon" src="<?php echo get_field('section_what_block-1_icon'); ?>" alt=""/>
+					<div class="home-block-what-item-title"> <?php echo get_field('section_what_block-1_title'); ?> </div>
+					<div class="home-block-what-item-text"> <?php echo get_field('section_what_block-1_text'); ?> </div>
 				</div>
-				<div class="col_one_third">
-					<img id="home-image-proximity-assets" src="<?php echo get_field('image_assets_home'); ?>" alt=""/>
-					<br>
-					<div id="home-title-proximity-assets" class="home3Tit">
-						<?php echo get_field('title_assets'); ?>
-					</div>
-					<div id="home-text-proximity-assets" class="home3Txt">
-						<?php echo get_field('text_assets'); ?>
-					</div>
+				<div class="clear"></div>
+				<div class="home-block-what-item">
+					<img class="home-block-what-item-icon" src="<?php echo get_field('section_what_block-2_icon'); ?>" alt=""/>
+					<div class="home-block-what-item-title"> <?php echo get_field('section_what_block-2_title'); ?> </div>
+					<div class="home-block-what-item-text"> <?php echo get_field('section_what_block-2_text'); ?> </div>
 				</div>
-				<div class="col_one_third col_last">
-					<img id="home-image-proximity-crowds" src="<?php echo get_field('image_crowds_home'); ?>" alt=""/>
-					<br>
-					<div id="home-title-proximity-crowds" class="home3Tit">
-						<?php echo get_field('title_crowds'); ?>
-					</div>
-					<div id="home-text-proximity-crowds" class="home3Txt">
-						<?php echo get_field('text_crowds'); ?>
-					</div>
+				<div class="clear"></div>
+			</div>
+			<div class="home-block-what-col">
+				<div class="home-block-what-item">
+					<img class="home-block-what-item-icon" src="<?php echo get_field('section_what_block-3_icon'); ?>" alt=""/>
+					<div class="home-block-what-item-title"> <?php echo get_field('section_what_block-3_title'); ?> </div>
+					<div class="home-block-what-item-text"> <?php echo get_field('section_what_block-3_text'); ?> </div>
 				</div>
-				<div>
-					<a href="/solutions/" id="home-button-proximity-solutions" class="button button-large no-bottom-margin btnSolutions" style="">
-						VIEW SOLUTIONS
-					</a>
+				<div class="clear"></div>
+				<div class="home-block-what-item">
+					<img class="home-block-what-item-icon" src="<?php echo get_field('section_what_block-4_icon'); ?>" alt=""/>
+					<div class="home-block-what-item-title"> <?php echo get_field('section_what_block-4_title'); ?> </div>
+					<div class="home-block-what-item-text"> <?php echo get_field('section_what_block-4_text'); ?> </div>
 				</div>
+				<div class="clear"></div>
 			</div>
 		</div>
 	</section>
-	<section id="before_clients" class="container-fluid center clearfix no-margin nopadding section-margin-fix home-page-section fp-auto-height">
-		<img id="home-image-background-before_clients" src="/wp-content/themes/sixgill/images/td/home/00_IconsSection_Lower.jpg" class="full-width" alt=""/>
+	<a name="products"></a>
+	<section id="products" class="container-fluid center clearfix no-margin nopadding section-margin-fix full-screen-height home-page-section fp-auto-height">
+		<div id="home-block-products-title" class="home-block-products-title"><?php echo get_field('text_productions'); ?></div>
+		<img id="home-image-infographic" src="<?php echo get_field('flow_chart_image'); ?>" class="home-block-products-diagram display-only-desktop" alt=""/>
+		<!-- 		<img id="home-image-infographic" src="/wp-content/themes/sixgill/images/4-Home/infographic.svg" class="home-block-products-diagram display-only-desktop" alt=""/> -->
+		<img id="home-image-mobile-infographic" src="<?php echo get_field('flow_chart_image_mobile_size'); ?>" class="home-diagram hidden-bottom-imageDiagram no-desktop-display" alt=""/>
+		<!-- 		<img id="home-image-mobile-infographic" src="/wp-content/themes/sixgill/images/4-Home/mobile_infographic.svg" class="home-diagram hidden-bottom-imageDiagram no-desktop-display" alt=""/> -->
+		<div class="clear"></div>
+		<div id="home-block-products-subtitle" class="home-block-products-subtitle"><?php echo get_field('before_button_text'); ?></div>
+		<a href="/products/" id="home-button-infographic" class="button button-blue">
+			View Products
+		</a>
+	</section>
+	<a name="solutions"></a>
+	<section id="solutions" class="clearfix center no-margin nopadding section-margin-fix full-screen-height home-page-section fp-auto-height" >
+		<div class="home-block-solutions-title">
+			<?php echo get_field('solution_title'); ?>
+		</div>
+		<div class="home-block-solutions-subtitle">
+			<?php echo get_field('solution_subtitle'); ?>
+		</div>
+		<a class="home-block-solutions-see-more-link" href="/solutions/">See All Use Cases</a>
+		<div id="home-block-solutions-carousel" class="home-block-solutions-carousel">
+			<?php query_posts
+			(array(
+				'post_parent' => 36,
+				'post_type' => 'page'
+				));
+			if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<div class="home-block-solutions-carousel-slide">
+					<div class="carousel-image-wrapper">
+						<?php the_post_thumbnail('home-solutions-slider');?>
+					</div>
+					<div class="carousel-text-wrapper">
+						<div class="carousel-text-caption-wrapper">
+							<span class="carousel-text-caption">
+								<?php the_title();?>
+							</span>
+						</div>
+						<div class="carousel-text-content-wrapper">
+							<hr class="carousel-text-content-line">
+							<div class="carousel-text-content">
+								<?php echo limit_words(get_field('first_section_text'), '35'); ?>
+								<?php// echo limit_words(get_field('first_section_text'), '27'); //why this value break all slider??>
+							</div>
+							<a href="<?php the_permalink(); ?>" class="carousel-text-content-link-more"> Learn More</a>
+						</div>
+					</div>
+				</div>
+			<?php endwhile; else: endif;  wp_reset_query(); ?>
+		</div>
 	</section>
 	<a name="clients"></a>
-	<section id="clients" class="color-white center section-margin-fix home-page-section fp-auto-height">
-		<div class="center home-clients-block">
-			<div class="fancy-title-without-bottom fancy-title title-dotted-border title-center mobile-hiden-line">
-				<h3 id="home-text-company-with-line">
-				<?php echo get_field('logos_title'); ?>
-				</h3>
-			</div>
-			<p class="logo-subheader">
-				<?php echo get_field('logos_subtitle'); ?>
-			</p>
-			<div class="home-logos">
-				<?php foreach(get_field('logos') as $logo) : ?><div class="home-logo-container center"><div class="vertical-centered"><img src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>" /></div></div><?php endforeach; ?>
-				<div class="home-logo-container full-width no-margin">
+	<section id="clients" class="center section-margin-fix home-page-section fp-auto-height">
+		<hr class="home-block-clients-line-before-title">
+		<div class="home-block-clients-title">
+			<?php echo get_field('logos_title'); ?>
+		</div>
+		<p class="home-block-clients-subtitle">
+			<?php echo get_field('logos_subtitle'); ?>
+		</p>
+		<div class="home-logos">
+			<?php foreach(get_field('logos') as $logo) : ?><div class="home-logo-container center"><div class="vertical-centered"><img src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>" /></div></div><?php endforeach; ?>
+			<!-- 			<div class="home-logo-container full-width no-margin">
 					<a href="/company/" id="home-button-image-company" class="button button-large no-bottom-margin btnCompany">
-						VIEW COMPANY
+							VIEW COMPANY
 					</a>
-				</div>
-			</div>
+			</div> -->
 		</div>
 	</section>
 	</section><!-- #content end -->
