5e4410150e0f6ed2df43f9282b7701cb6478d71d
vterekhov <haosx86@gmail.com>
2016-08-04 15:50:30 +0300

    implement desktop view

--- wp-content/themes/sixgill/page-tech.php
+++ wp-content/themes/sixgill/page-tech.php
@@ -46,29 +46,78 @@
 		</div>
 	</div>
 
-	<a name="products-second-section"></a>
-	<div id="products-second-section" class="products-section products-second-section">
-		<div class="products-second-section-wrapper">
-			<div class="products-second-section-left-col">
-				<div class="products-second-section-image-wrapper">
+	<a name="tech-second-section"></a>
+	<div id="tech-second-section" class="tech-section tech-second-section">
+		<div class="tech-second-section-wrapper">
+			<div class="tech-second-section-left-col">
+				<div class="tech-second-section-image-wrapper">
 					<img
-						class="products-second-section-image responsive-image"
+						class="tech-second-section-image responsive-image"
 						desktop-src="<?php echo get_field('second_section_image_desktop'); ?>"
 						tablet-src="<?php echo get_field('second_section_image_tablet'); ?>"
 						mobile-src="<?php echo get_field('second_section_image_mobile'); ?>"
 					>
 				</div>
 			</div>
-			<div class="products-second-section-right-col">
-				<div class="products-second-section-content-wrapper">
-					<div class="products-second-section-title">
+			<div class="tech-second-section-right-col">
+				<div class="tech-second-section-content-wrapper">
+					<div class="tech-second-section-title">
 						<?php echo get_field('second_section_title'); ?>
 					</div>
-					<div class="products-second-section-text">
+					<div class="tech-second-section-text">
 						<?php echo get_field('second_section_text'); ?>
 					</div>
 				</div>
 			</div>
+		</div>
+	</div>
+
+	<a name="tech-third-section"></a>
+	<div id="tech-third-section" class="tech-section tech-third-section">
+		<div class="tech-third-section-wrapper">
+
+			<div class="tech-third-section-left-col">
+				<div class="tech-third-section-content-wrapper">
+					<div class="tech-third-section-title">
+						<?php echo get_field('third_section_title'); ?>
+					</div>
+
+					<div class="tech-third-section-text">
+						<?php echo get_field('third_section_text'); ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<a name="tech-fourth-section"></a>
+	<div id="tech-fourth-section" class="tech-section tech-fourth-section">
+		<div class="tech-fourth-section-wrapper">
+
+			<div class="tech-fourth-section-left-col">
+				<div class="tech-fourth-section-image-wrapper">
+					<img
+						class="tech-fourth-section-image responsive-image"
+						desktop-src="<?php echo get_field('fourth_section_image_desktop'); ?>"
+						tablet-src="<?php echo get_field('fourth_section_image_tablet'); ?>"
+						mobile-src="<?php echo get_field('fourth_section_image_mobile'); ?>"
+					>
+				</div>
+			</div>
+
+			<div class="tech-fourth-section-right-col">
+				<div class="tech-fourth-section-content-wrapper">
+					<div class="tech-fourth-section-title">
+						<?php echo get_field('fourth_section_title'); ?>
+					</div>
+
+					<div class="tech-fourth-section-text">
+						<?php echo get_field('fourth_section_text'); ?>
+					</div>
+				</div>
+			</div>
+
+
 		</div>
 	</div>
 
