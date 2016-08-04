22aa8e860c6f88df32bc2735803b05e2a88cf381
Daniil Leksin <danilaleksin@gmail.com>
2016-08-04 15:40:58 +0300

    Fix menu

--- wp-content/themes/sixgill/js/custom/1_mobile_menu.js
+++ wp-content/themes/sixgill/js/custom/1_mobile_menu.js
@@ -1,28 +1,37 @@
 jQuery(function($) {
 	window.menuShowFlag = false;
+	var flag = false
 	var savedScroll;
 
 	$('#primary-menu-trigger').click(function() {
-		if(window.menuShowFlag) {
+		if(flag) {
 			$( '#menu-background' ).fadeTo(500, 0, function() {
-				$( '#primary-menu > ul, #menu-background' ).toggleClass("show");
+				// $( '#primary-menu > ul, #menu-background' ).toggleClass("show");
 			});
+
 			$( '#primary-menu' ).fadeTo(400, 0, function() {
 				$(window).scrollTop(savedScroll);
+
 			});
 		} else {
 			savedScroll = $(window).scrollTop();
-			$( '#primary-menu > ul, #menu-background' ).toggleClass("show");
-			$( '#primary-menu, #menu-background' ).fadeTo(500, 1);
+			// $( '#primary-menu > ul, #menu-background' ).toggleClass("show");
+			// $( '#primary-menu, #menu-background' ).fadeTo(500, 1);
 		}
 		$('#mobile-table-search').toggleClass("show");
 
-		window.menuShowFlag = !window.menuShowFlag;
+		flag = !flag;
 
-		$('section').toggleClass('hide-landscape-mobile');
-		$('footer').toggleClass('hide-landscape-mobile');
+		window.menuShowFlag = flag;
 
-		$( '#show-menu-icon, #close-menu-icon' ).toggleClass("hide");
+		$('section').toggleClass('hide');
+		$(' footer ').toggleClass('hide');
+		$( '#content' ).toggleClass("hide");
+				
+
+		// $( '#show-menu-icon, #close-menu-icon' ).toggleClass("hide");
 		return false;
 	});
+
+
 });
