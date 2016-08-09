56f69040892bfd020205ed474c132efdc1499123
haosx86 <haosx86@gmail.com>
2016-08-09 08:39:54 +0300

    in progress

--- wp-content/themes/sixgill/page-home.php
+++ wp-content/themes/sixgill/page-home.php
@@ -19,10 +19,6 @@
 </a>
 
 <div id="home-fullpage-wrapper" class="full-width">
-
-
-
-	<a name="home-first-section"></a>
 	<div id="home-first-section" class="home-section home-first-section">
 		<video autoplay loop muted id="home-video">
 			<source src="<?php echo get_field('video_url'); ?>" type="video/mp4">
@@ -32,7 +28,7 @@
 
 		<div class="home-first-section-block">
 			<div class="home-first-section-block-text">
-				<div id="home-text-on-video" data-animate="fadeInDown" class="txtVideo home-video-text-h1">
+				<div class="home-first-section-text">
 					<?php echo get_field('blue_header_text_below_video'); ?>
 				</div>
 				<button
@@ -47,23 +43,175 @@
 		</div>
 	</div>
 
-	<div id="home-section-what" class="home-section home-section-what">
-		<div class="home-block-what-title-wrapper">
-			<div class="home-block-what-subtitle-on-line">
-				<?php echo get_field('the_text_on_the_line'); ?>
+	<div id="home-second-section" class="home-section home-second-section">
+		<div class="home-second-section-wrapper">
+			<div class="home-second-section-header-wrapper">
+				<div class="home-second-section-header-top">
+					<div class="home-second-section-header-top-line"></div>
+					<div class="home-second-section-header-top-title">
+						We are experts in helping you
+					</div>
+					<div class="home-second-section-header-top-line"></div>
+				</div>
+				<h2 class="home-second-section-title">
+					TRACK, DETERMINE AND ACT
+				</h2>
+				<div class="home-second-section-subtitle">
+					on the ever-changing relationships between people, places and things essential to your success.
+				</div>
 			</div>
-			<hr class="hr-home-suptitle-line no-mobile-portrait-display no-mobile-landscape-display" />
-			<div class="home-block-what-title">
-				<?php echo get_field('large_text_title'); ?>
+			<div class="home-second-section-content-wrapper">
+				<div class="home-second-section-row">
+					<div class="home-second-section-block">
+						<div class="home-second-section-icon-wrapper">
+							<img class="home-second-section-icon" src="">
+						</div>
+						<div class="home-second-section-block-content">
+							<h3 class="home-second-section-block-title">
+								Drive Business Value
+							</h3>
+							With our market-proven enterprise proximity platform, customers can Track the key people, places, and things that matter to them, Determine proximity and intent, and Act to drive business value.
+						</div>
+					</div>
+
+					<div class="home-second-section-block">
+						<div class="home-second-section-icon-wrapper">
+							<img class="home-second-section-icon" src="">
+						</div>
+						<div class="home-second-section-block-content">
+							<h3 class="home-second-section-block-title">
+								Enterprise proximity platform
+							</h3>
+							Our proximity engine—at the heart of the platform—has normalized 1.5 billion multidimensional measurements and triggered 25 million context-sensitive actions for our customers. And counting.
+						</div>
+					</div>
+				</div>
+
+				<div class="home-second-section-row">
+					<div class="home-second-section-block">
+						<div class="home-second-section-icon-wrapper">
+							<img class="home-second-section-icon" src="">
+						</div>
+						<div class="home-second-section-block-content">
+							<h3 class="home-second-section-block-title">
+								TURN MOBILITY INTO AN ASSET
+							</h3>
+							We turn the increasing mobility of the enterprise workforce and the connected nature of the Internet of Things into a business asset, converting vast amounts of location and sensor data into understandable analytics and targeted action.
+						</div>
+					</div>
+
+					<div class="home-second-section-block">
+						<div class="home-second-section-icon-wrapper">
+							<img class="home-second-section-icon" src="">
+						</div>
+						<div class="home-second-section-block-content">
+							<h3 class="home-second-section-block-title">
+								ONE FOCUS
+							</h3>
+							At Sixgill we have one focus—to enable our customers to embrace mobility as an asset while maintaining the control they need to maximize value.
+						</div>
+					</div>
+				</div>
+
 			</div>
-			<div class="home-block-what-subtitle">
-				<?php echo get_field('the_text_below_the_title'); ?>
-			</div>
-			<hr class="hr-home-suptitle-line" />
 		</div>
+	</div>
 
+	<div id="home-third-section" class="home-section home-third-section">
+		<div class="home-third-section-wrapper">
+			<h2 class="home-third-section-title">
+					Sixgill SenseTM tracks, determines, and acts on the changing proximity of people, places and things.
+			</h2>
+			<div class="home-third-section-image-wrapper">
+				<img class="home-third-section-image" src="">
+			</div>
+			<div class="home-third-section-bottom-part">
+				<div class="home-third-section-bottom-part-text">
+					See what the Sixgill Sense Platform can do for your company.
+				</div>
+				<a href="/products/" class="button button-blue home-third-section-button">
+					View Products
+				</a>
+			</div>
+		</div>
+	</div>
 
+	<div id="home-fourth-section" class="home-section home-fourth-section">
+		<div class="home-fourth-section-wrapper">
+			<div class="home-fourth-section-header-wrapper">
+				<h2 class="home-fourth-section-title">
+					Proximity for the People, Assets and Crowds
+				</h2>
+				<div class="home-fourth-section-subtitle">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit incidunt ut labore et dolore magna alique minim veniam.
+				</div>
+				<a href="/solutions/" class="home-fourth-section-link">
+					See All Use Cases
+				</a>
+			</div>
+			<div class="home-fourth-section-carousel-wrapper">
 
-	</section>
+			</div>
+		</div>
+	</div>
+
+	<div id="home-fifth-section" class="home-section home-fifth-section">
+		<div class="home-fifth-section-wrapper">
+			<div class="home-fifth-section-header-wrapper">
+				<div class="home-fifth-section-dash"></div>
+				<h2 class="home-fifth-section-title">
+					PROVEN AT CONSUMER VOLUMES
+				</h2>
+				<div class="home-fifth-section-subtitle">
+					Sixgill provides proximity enablement for mobile apps including mobile program customers of Lemon.
+				</div>
+			</div>
+
+			<div class="home-fifth-section-logos-wrapper">
+				<!-- First row -->
+				<div class="home-fifth-section-logo-wrapper">
+					<img class="home-fifth-section-logo" src="">
+				</div>
+
+				<div class="home-fifth-section-logo-wrapper">
+					<img class="home-fifth-section-logo" src="">
+				</div>
+
+				<div class="home-fifth-section-logo-wrapper">
+					<img class="home-fifth-section-logo" src="">
+				</div>
+
+				<div class="home-fifth-section-logo-wrapper">
+					<img class="home-fifth-section-logo" src="">
+				</div>
+
+				<div class="home-fifth-section-logo-wrapper">
+					<img class="home-fifth-section-logo" src="">
+				</div>
+
+				<!-- Second row -->
+
+				<div class="home-fifth-section-logo-wrapper">
+					<img class="home-fifth-section-logo" src="">
+				</div>
+
+				<div class="home-fifth-section-logo-wrapper">
+					<img class="home-fifth-section-logo" src="">
+				</div>
+
+				<div class="home-fifth-section-logo-wrapper">
+					<img class="home-fifth-section-logo" src="">
+				</div>
+
+				<div class="home-fifth-section-logo-wrapper">
+					<img class="home-fifth-section-logo" src="">
+				</div>
+
+				<div class="home-fifth-section-logo-wrapper">
+					<img class="home-fifth-section-logo" src="">
+				</div>
+			</div>
+		</div>
+	</div>
 
 <?php get_footer(); ?>
