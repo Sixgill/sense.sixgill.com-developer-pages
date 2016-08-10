8d6926e47ca077d491ef3479af8bf67a32b84a4f
haosx86 <haosx86@gmail.com>
2016-08-10 19:22:24 +0300

    add tablet version

--- wp-content/themes/sixgill/js/custom/1_home_solutions_carousel.js
+++ wp-content/themes/sixgill/js/custom/1_home_solutions_carousel.js
@@ -1,29 +1,24 @@
 jQuery(function($){
+	$("#home-fourth-section-carousel").owlCarousel({
+		// Most important owl features
+		items : 3, //3 items above 1200px browser width
+		itemsDesktop : [1200,3], //3 items between 1200px and 300px
+		itemsDesktopSmall : false, // disabled - inherit from itemsDesktop option
+		itemsTablet: [1100,2], //2 items between 1200 and 600
+		itemsMobile : [500,1], //1 items between 600 and 0
+		// Navigation
+		navigation : true,
+		navigationText : ["",""],
 
-	if(window.screenType==="desktop") {
-		console.log('Starting home page carousel...', window.screenType);
-		$("#home-fourth-section-carousel").owlCarousel({
-			// Most important owl features
-			items : 3, //3 items above 1200px browser width
-			itemsDesktop : [1200,3], //3 items between 1200px and 300px
-			itemsDesktopSmall : false, // disabled - inherit from itemsDesktop option
-			itemsTablet: [1100,2], //2 items between 1200 and 600
-			itemsMobile : [500,1], //1 items between 600 and 0
-			// Navigation
-			navigation : true,
-			navigationText : ["",""],
+		//Pagination
+		pagination : false,
 
-			//Pagination
-			pagination : false,
+		// // Responsive
+		responsive: true,
+		// responsiveRefreshRate : 200,
+		//responsiveBaseWidth: "#home-block-solutions-carousel",
 
-			// // Responsive
-			responsive: true,
-			// responsiveRefreshRate : 200,
-			//responsiveBaseWidth: "#home-block-solutions-carousel",
+		theme : "home-fourth-section-carousel",
 
-			theme : "home-fourth-section-carousel",
-
-		});
-	}
-
+	});
 });
