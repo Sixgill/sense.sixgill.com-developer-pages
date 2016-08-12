145df1d7bab9a44cbb5c6ecda6c4ac8ddb6d7bf7
Michael Nazarov <mnazarov@WeezLabs.com>
2016-08-12 17:08:35 +0300

    different adaptive fixes for company page

--- wp-content/themes/sixgill/page-company.php
+++ wp-content/themes/sixgill/page-company.php
@@ -35,205 +35,66 @@
 			</div>
 		</div>
 	</div>
-	<!-- <a name="company-second-section"></a> -->
 	<a name="leadership"></a>
 	<div class="company-section company-second-section">
 		<div class="inline-block full-width">
 			<div class="company-second-section-block">
 			<h1 class="company-second-section-block-title">
-				Leadership
+				<?php echo get_field('leadership_title'); ?>
 			</h1>
-			<div class="company-second-section-team">
-				<div class="company-second-section-rows">
-					<div class="company-second-section-image-wrapper">
-						<img class="company-second-section-image" src="/wp-content/themes/sixgill/images/company/tbnPhil@1x.png">
-					</div>
-					<div class="company-second-section-team-description">
-						<div class="company-second-section-row-item">
-							<p class="company-second-section-name">Phil Ressler</p>
+			<?php foreach(get_field('leadership_staff') as $team) : ?>
+				<div class="company-second-section-team">
+					<div class="company-second-section-rows">
+						<div class="company-second-section-image-wrapper">
+							<img class="company-second-section-image" src="<?php echo $team['photo']; ?>">
 						</div>
-						<div class="company-second-section-row-item">
-							<p class="company-second-section-job">Chief Executive Officer</p>
-						</div>
-						<div class="company-second-section-row-item">
-							<p class="company-second-section-bio">Phil has been serially involved in building companies and expanding markets for a wide range of innovators in enterprise data availability, comprehension and collaboration spanning the mobile, web, networking and PC eras.</p>
-							<p class="company-second-section-bio-full"><br></p>
-							<p class="company-second-section-more-trigger">
-								<a class="learn-more-link">
-									Learn More
-									<span class="learn-more-expand-icon">
-										&or;
-									</span>
-									<span class="learn-more-close-icon">
-										&and;
-									</span>
-								</a>
-							</p>
-						</div>
-					</div>
-				</div>
-				<div class="company-second-section-rows">
-					<div class="company-second-section-image-wrapper">
-						<img class="company-second-section-image" src="/wp-content/themes/sixgill/images/company/tbnJohn@1x.png">
-					</div>
-					<div class="company-second-section-team-description">
-						<div class="company-second-section-row-item">
-							<p class="company-second-section-name">John Dohm</p>
-						</div>
-						<div class="company-second-section-row-item">
-							<p class="company-second-section-job">Chief Technology Officer</p>
-						</div>
-						<div class="company-second-section-row-item">
-							<p class="company-second-section-bio">John began is career as a software engineer for internal enterprise systems and spiraled away from his keyboard enough to gather up an ever-widening range of technology creation, scaling, architecture, deployment, operations and and general executive experiences. In a succession of companies including Quaker, Deloitte, American Standard, Intellos, Guru Denim and RealD, John worked on networking infrastructure, internal ERP, inventory, CRM and HR systems, vehicle control and telematics systems, and managed global IT operations.</p>
-							<p class="company-second-section-bio-full"><br></p>
-							<p class="company-second-section-more-trigger">
-								<a class="learn-more-link">
-									Learn More
-									<span class="learn-more-expand-icon">
-										&or;
-									</span>
-									<span class="learn-more-close-icon">
-										&and;
-									</span>
-								</a>
-							</p>
+						<div class="company-second-section-team-description">
+							<div class="company-second-section-row-item">
+								<p class="company-second-section-name"><?php echo $team['name']; ?></p>
+							</div>
+							<div class="company-second-section-row-item">
+								<p class="company-second-section-job"><?php echo $team['title']; ?></p>
+							</div>
+							<div class="company-second-section-row-item">
+								<div class="company-second-section-bio"><?php echo $team['short_bio']; ?></div>
+								<div class="company-second-section-bio-full"><br><?php echo $team['more_bio']; ?></div>
+								<p class="company-second-section-more-trigger">
+									<a class="learn-more-link">
+										<span class="learn-more-title">Learn More</span>
+										<span class="learn-more-expand-icon">
+											&or;
+										</span>
+										<span class="learn-more-close-icon">
+											&and;
+										</span>
+									</a>
+								</p>
+							</div>
 						</div>
 					</div>
 				</div>
-				<div class="company-second-section-rows">
-					<div class="company-second-section-image-wrapper">
-						<img class="company-second-section-image" src="/wp-content/themes/sixgill/images/company/tbnJeff@1x.png">
-					</div>
-					<div class="company-second-section-team-description">
-						<div class="company-second-section-row-item">
-							<p class="company-second-section-name">Jeff Ester</p>
-						</div>
-						<div class="company-second-section-row-item">
-							<p class="company-second-section-job">Senior Vice President, Revenue</p>
-						</div>
-						<div class="company-second-section-row-item">
-							<p class="company-second-section-bio">Jeff has built an extended career as the pioneering revenue strategist and instigator in early-stage companies just launching into ungelled markets where innovators find themselves on the cusp of explosive growth.</p>
-							<p class="company-second-section-bio-full">He has proven able to build and lead success-driving revenue teams and lay in selling infrastructure that gives young enterprise companies scale and growth. Jeff began his career learning disciplined enterprise sales at Oracle and took his newly-acquired expertise to then-startup Pure Software. He pioneered enterprise product sales for Netscape, continuing to serve AOL after acquisition. After over a decade incubating and advising a plethora of technology startups, Jeff returned to the operating world to build and lead the sales function at customer intelligence innovator EdgeTheory, prior to joining Zos Communications and its transformation into Sixgill.</p>
-							<p class="company-second-section-more-trigger">
-								<a class="learn-more-link">
-									Learn More
-									<span class="learn-more-expand-icon">
-										&or;
-									</span>
-									<span class="learn-more-close-icon">
-										&and;
-									</span>
-								</a>
-							</p>
-						</div>
-					</div>
-				</div>
-				<div class="company-second-section-rows">
-					<div class="company-second-section-image-wrapper">
-						<img class="company-second-section-image" src="/wp-content/themes/sixgill/images/company/tbnMike@1x.png">
-					</div>
-					<div class="company-second-section-team-description">
-						<div class="company-second-section-row-item">
-							<p class="company-second-section-name">Mike Horowitz</p>
-						</div>
-						<div class="company-second-section-row-item">
-							<p class="company-second-section-job">Senior Vice President, Product Management</p>
-						</div>
-						<div class="company-second-section-row-item">
-							<p class="company-second-section-bio">
-								If you use any of the popular cloud-based platform applications available on the web or bought an event ticket online, you’ve probably used something cool that Mike built or influenced.
-							</p>
-							<p class="company-second-section-bio-full">
-								As a product leader building innovative consumer and enterprise applications for mobile and the web, Mike has indirectly reached hundreds of millions of customers through raw startups Applied Semantics, Fetch and RadicalMail to established titans Google (AdSense, Picasa, Google Apps), Microsoft (FrontPage) and Ticketmaster (client and consumer apps). Having worked on everything from enterprise software, machine learning, communications, messaging, email, structured data, consumer ecommerce, and machine vision systems deployed from hugely-scalable platforms, Mike is uniquely qualified to lead the evolution of Sixgill Sense from today’s nascent proximity applications to the coming surge of device populations and streaming instructions in the billions, reliant on real-time proximity comprehension.
-							</p>
-							<p class="company-second-section-more-trigger">
-								<a class="learn-more-link">
-									Learn More
-									<span class="learn-more-expand-icon">
-										&or;
-									</span>
-									<span class="learn-more-close-icon">
-										&and;
-									</span>
-								</a>
-							</p>
-						</div>
-					</div>
-				</div>
-				<div class="company-second-section-rows">
-					<div class="company-second-section-image-wrapper">
-						<img class="company-second-section-image" src="/wp-content/themes/sixgill/images/company/tbnShawn@1x.png">
-					</div>
-					<div class="company-second-section-team-description">
-						<div class="company-second-section-row-item">
-							<p class="company-second-section-name">Shawn Gunn</p>
-						</div>
-						<div class="company-second-section-row-item">
-							<p class="company-second-section-job">Senior Vice President, Business Development</p>
-						</div>
-						<div class="company-second-section-row-item">
-							<p class="company-second-section-bio">Shawn has accumulated nearly unparalleled mobile, location services and movement analytics expertise serving as ecosystem builder at a succession of mobility markets innovators.</p>
-							<p class="company-second-section-bio-full">After beginning his career as a financial trader, Shawn built his mobility career brick-by-brick leading business development and strategic partnership functions at Globalspec, Traffic.com, NAVTEQ and Nokia, with a detour as a consultant at Hearst Digital along the way. Not merely a senior relationship executive, Shawn brings Sixgill a recurring history of connecting the ecosystem building of business development to hard revenue, and monetization of ancillary services.</p>
-							<p class="company-second-section-more-trigger">
-								<a class="learn-more-link">
-									Learn More
-									<span class="learn-more-expand-icon">
-										&or;
-									</span>
-									<span class="learn-more-close-icon">
-										&and;
-									</span>
-								</a>
-							</p>
-						</div>
-					</div>
-				</div>
-			</div>
+			<?php endforeach; ?>
 			</div>
 		</div>
 	</div>
-	<!-- <a name="company-third-section"></a> -->
 	<a name="board"></a>
 	<div class="company-section company-third-section">
 		<div class="inline-block full-width">
 			<div class="company-third-section-text">
 				<h1 class="company-third-section-title">
-					The Board
+					<?php echo get_field('board_title'); ?>
 				</h1>
 			</div>
 			<div class="company-third-section-rows">
-				<div class="company-third-section-left company-third-section-row-item">
-					<p class="company-third-section-name">Harry Hopper</p>
-					<p class="company-third-section-job">Executive Chairman</p>
-					<ul>
-						<li class="company-third-section-bio">25+ years investing inTelecom/Mobile/IT</li>
-						<li class="company-third-section-bio">Columbia Capital, Senior Advisor(formerly Managing Partner andPartner 20 yrs; $2.5B under
-					   management)</li>
-						<li class="company-third-section-bio">President telecom investment bank, Entrepreneur/Founder 2 venture backed</li>
-					   <li class="company-third-section-bio">
-					   Boards: Open Mobile, Mobile Posse;
-					   LendPro. Former: MetroPCS (PCS);
-					   Equinix (EQIX), Pegasus (PGTV),
-					   DSL.net (DSLN): Private 5 mobile
-					   operators, 10 Media, IT Service Co.’s</li>
-					   <li class="company-third-section-bio">Forbes Midas List</li>
-					</ul>
-				</div>
-				<div class="company-third-section-center company-third-section-row-item">
-					<p class="company-third-section-name">Phil Ressler</p>
-					<p class="company-third-section-job">CEO</p>
-					<p class="company-third-section-bio">Phil has been serially involved in building companies and expanding markets for a wide range of innovators in enterprise data availability, comprehension and collaboration spanning the mobile, web, networking and PC eras. See Phil’s full bio above in Team.
-					</p>
-				</div>
-				<div class="company-third-section-right company-third-section-row-item">
-					<p class="company-third-section-name">Christopher Rogers</p>
-					<p class="company-third-section-job">Director</p>
-					<ul>
-						<li class="company-third-section-bio">Co-Founder of Nextel Communications</li>
-						<li class="company-third-section-bio">Director at LightSquared, PlayStar,and the Wireless Board Registry</li>
-						<li class="company-third-section-bio">Partner at Lumia Capital</li>
-					</ul>
-				</div>
+				<?php foreach(get_field('board') as $board) : ?>
+					<div class="company-third-section-left company-third-section-row-item">
+						<p class="company-third-section-name"><?php echo $board['name']; ?></p>
+						<p class="company-third-section-job"><?php echo $board['title']; ?></p>
+						<div class="company-third-section-bio">
+							<?php echo $board['bio']; ?>
+						</div>
+					</div>
+				<?php endforeach; ?>
 			</div>
 		</div>
 	</div>
