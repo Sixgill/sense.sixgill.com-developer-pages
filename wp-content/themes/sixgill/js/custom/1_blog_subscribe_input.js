299768efe20e1086ed399624f8f80abfdd8b1843
vterekhov <haosx86@gmail.com>
2016-09-01 14:42:43 +0300

    in progress

--- wp-content/themes/sixgill/js/custom/1_blog_subscribe_input.js
+++ wp-content/themes/sixgill/js/custom/1_blog_subscribe_input.js
@@ -1,9 +1,9 @@
 jQuery(function($){
-	$('input.subscribe-email').focusin(function(){
+	$('input.blog-page-subscription-email').focusin(function(){
 		$(this).addClass('subscribe-email-focus');
 	});
 
-	$('input.subscribe-email').focusout(function(){
+	$('input.blog-page-subscription-email').focusout(function(){
 		if($(this).val()==='') {
 			$(this).removeClass('subscribe-email-focus');
 		}
