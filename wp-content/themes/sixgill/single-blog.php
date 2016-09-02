f0d59934b1a2ad303b2e73aefc1bf6fdc1029b53
vterekhov <haosx86@gmail.com>
2016-09-02 13:12:38 +0300

    in progress

--- wp-content/themes/sixgill/single-blog.php
+++ wp-content/themes/sixgill/single-blog.php
@@ -1,7 +1,30 @@
 <div class="full-width ohidden"> <!-- fullpage wrapper placeholder -->
 	<a name="top"></a>
-<?php
-	if ( have_posts() ) while ( have_posts() ) : the_post();
-?>
 
-<?php endwhile; // end of the loop. ?>
+	<div
+		class="blog-post-header responsive-background"
+		desktop-src=""
+		tablet-src=""
+		mobile-src=""
+	>
+		<div class="blog-post-header-wrapper">
+			<h1 class="blog-post-header-title">
+				How to keep everything under control when you have a successful start-up
+			</h1>
+			<div class="blog-post-header-subtitle">
+				PHIL RESSLER  |  08/19/2016
+			</div>
+		</div>
+	</div>
+
+	<div class="blog-post-content-wrapper">
+		<?php
+			if ( have_posts() ) while ( have_posts() ) : the_post();
+		?>
+
+		
+
+		<?php endwhile; // end of the loop. ?>
+	</div>
+
+	<!-- TODO: Implement next and prev buttons -->
