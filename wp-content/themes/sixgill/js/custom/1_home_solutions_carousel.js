4c72d8a9aa7509bd0be9f8380060f49b97c3f083
Ilya Napastyuk <napastyuk@gmail.com>
2016-09-07 19:38:11 +0300

    carusel width fixing

--- wp-content/themes/sixgill/js/custom/1_home_solutions_carousel.js
+++ wp-content/themes/sixgill/js/custom/1_home_solutions_carousel.js
@@ -1,11 +1,11 @@
 jQuery(function($){
 	$("#home-fourth-section-carousel").owlCarousel({
 		// Most important owl features
-		items : 3, //3 items above 1200px browser width
-		itemsDesktop : [921,3], //3 items between 1200px and 300px
+		items : 4, //3 items above 1200px browser width
+		itemsDesktop : [1200,3], //3 items between 1200px and 921px
 		itemsDesktopSmall : false, // disabled - inherit from itemsDesktop option
-		itemsTablet: [920,2], //2 items between 1200 and 600
-		itemsMobile : [640,1], //1 items between 600 and 0
+		itemsTablet: [1199,2], //2 items between 920 and 640
+		itemsMobile : [500,1], //1 items between 640 and 0
 		// Navigation
 		navigation : true,
 		navigationText : ["",""],
