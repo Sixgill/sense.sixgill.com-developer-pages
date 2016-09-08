01d69ecd03a41583a12fada0fa09b84d4ce83939
Ilya Napastyuk <napastyuk@gmail.com>
2016-09-08 14:26:40 +0300

    task in progress

--- wp-content/themes/sixgill/js/custom/1_home_solutions_carousel.js
+++ wp-content/themes/sixgill/js/custom/1_home_solutions_carousel.js
@@ -1,8 +1,8 @@
 jQuery(function($){
 	$("#home-fourth-section-carousel").owlCarousel({
 		// Most important owl features
-		items : 4, //3 items above 1200px browser width
-		itemsDesktop : [1200,3], //3 items between 1200px and 921px
+		items : 5, //3 items above 1200px browser width
+		itemsDesktop : [1200,4], //3 items between 1200px and 921px
 		itemsDesktopSmall : false, // disabled - inherit from itemsDesktop option
 		itemsTablet: [1199,2], //2 items between 920 and 640
 		itemsMobile : [500,1], //1 items between 640 and 0
