5b6152ae657a59219f75bfcea559ce3d4cfb92df
vterekhov <haosx86@gmail.com>
2016-09-08 18:27:05 +0300

    merge SIX-534

--- wp-content/themes/sixgill/single-blog.php
+++ wp-content/themes/sixgill/single-blog.php
@@ -3,24 +3,37 @@
 
 	<div
 		class="blog-post-header responsive-background"
-		desktop-src="<?php echo get_field("header_image_desktop"); ?>"
-		tablet-src="<?php echo get_field("header_image_tablet"); ?>"
-		mobile-src="<?php echo get_field("header_image_mobile"); ?>"
+		<?php
+			$filterFlag = get_field('blue_filter', false, false);
+			$desktop = get_field("header_image_desktop");
+			$tablet = get_field("header_image_tablet");
+			$mobile = get_field("header_image_mobile");
+
+			echo 'desktop-src='.$desktop.' tablet-src='.$tablet.' mobile-src='.$mobile;
+		?>
+
+
 	>
-		<h1 class="blog-post-header-title ellipsis-3lines">
-			<?php echo $post->post_title; ?>
-		</h1>
-		<div
-			class="blog-post-header-subtitle
-				ellipsis-2lines-desktop ellipsis-3lines-tablet ellipsis-2lines-mobile-portrait ellipsis-2lines-mobile-landscape"
-		>
-			<?php echo get_field('subtitle'); ?>
+		<div class="blog-post-header-filter <?php echo $filterFlag == '1' ? '' : 'hide'?>"></div>
+		<div class="blog-post-header-contaier">
+			<h1 class="blog-post-header-title ellipsis-3lines">
+				<?php echo $post->post_title; ?>
+			</h1>
+			<div
+				class="blog-post-header-subtitle
+					ellipsis-2lines-desktop ellipsis-3lines-tablet ellipsis-2lines-mobile-portrait ellipsis-2lines-mobile-landscape"
+			>
+				<?php echo get_field('subtitle'); ?>
+
+			</div>
 		</div>
 	</div>
 
 	<div class="blog-post-content-wrapper">
 		<?php include(locate_template('social_buttons.php')); ?>
+
 		<div class="blog-post-content-wrapper-line no-tablet-display no-desktop-display"></div>
+
 		<div class="blog-post-author-date">
 			<?php the_author(); ?> | <?php the_date('m/d/Y'); ?>
 		</div>
@@ -59,5 +72,3 @@
 
 		<?php wpb_get_post_views(get_the_ID()); endwhile; wp_reset_query(); // end of the loop. ?>
 	</div>
-
-	<!-- TODO: Implement next and prev buttons -->
