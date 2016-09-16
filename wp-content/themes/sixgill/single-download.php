6c5aff2fb943be4e118bb749e44d711e23a958e3
vterekhov <haosx86@gmail.com>
2016-09-16 18:14:20 +0300

    add header for resource details page

--- wp-content/themes/sixgill/single-download.php
+++ wp-content/themes/sixgill/single-download.php
@@ -1,11 +1,22 @@
-<div
-	class="resource-page-header responsive-background"
-	desktop-src=""
-	tablet-src=""
-	mobile-src=""
-></div>
+<?php
+	$resourcePageID = getPageBySlug('resources')->ID;
+?>
 <?php
 	if ( have_posts() ) while ( have_posts() ) : the_post();
 ?>
-	<?php the_content(); ?>
+<div
+	class="resources-list-header responsive-background"
+	desktop-src="<?php the_field("header_image_desktop", $resourcePageID); ?>"
+	tablet-src="<?php the_field("header_image_tablet", $resourcePageID); ?>"
+	mobile-src="<?php the_field("header_image_mobile", $resourcePageID); ?>"
+>
+	<h1 class="resources-list-header-title">
+		SIXGILL RESOURCES -
+		<?php echo wp_get_post_terms($id, 'sdm_categories')[0]->name; ?>
+	</h1>
+	<h2 class="resources-list-header-subtitle">
+		<?php the_title(); ?>
+	</h2>
+</div>
+
 <?php endwhile; // end of the loop. ?>
