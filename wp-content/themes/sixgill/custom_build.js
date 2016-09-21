32613a2ea371dcef30884e8f556047f853711a75
vterekhov <haosx86@gmail.com>
2016-09-21 18:19:30 +0300

    improve downloading

--- wp-content/themes/sixgill/custom_build.js
+++ wp-content/themes/sixgill/custom_build.js
@@ -6402,20 +6402,18 @@ jQuery(function($) {
 			successMessageSelector = formContainerClassSelector + ' .mc4wp-success',
 			noticeMessageSelector = formContainerClassSelector + ' .mc4wp-notice',
 			errorMessageSelector = formContainerClassSelector + ' .mc4wp-error',
-			downloadLink = $('.resource-details').attr('data-download-link');
+			downloadLink = $('.resource-details').attr('data-download-link'),
+			downloadInfo = $('.resource-details-download-link');
 
 	function watchSuccess() {
-		console.log('watchSuccess');
 		var successMessageContainer = $(successMessageSelector);
 		var errorMessageContainer = $(noticeMessageSelector + ', ' + errorMessageSelector);
 		if(successMessageContainer.length) {
-			console.log('successMessage');
-			console.log('downloadLink '+ downloadLink);
 			window.open(downloadLink, "_self");
+			downloadInfo.removeClass('hide');
 		}
 
 		if (successMessageContainer.length || errorMessageContainer.length) {
-			console.log('success or error message');
 			submittedFlag = false;
 			return;
 		}
@@ -6423,7 +6421,6 @@ jQuery(function($) {
 	}
 
 	$(formSelector).on('submit', function() {
-		console.log('download form submit');
 		if(submittedFlag) return;
  		submittedFlag = true;
 		watchSuccess();
