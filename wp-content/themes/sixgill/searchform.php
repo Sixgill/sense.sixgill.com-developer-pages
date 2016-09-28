fe60be49c4eed6d0d1ec971c257bd34c583f72fb
vterekhov <haosx86@gmail.com>
2016-09-28 18:41:57 +0300

    in progress

--- wp-content/themes/sixgill/searchform.php
+++ wp-content/themes/sixgill/searchform.php
@@ -4,8 +4,8 @@
 	method="get"
 	action="<?php echo home_url('/'); ?>"
 >
-  <input type="text" class="search-form-input" required>
-  <input name="s" class="search-form-submit">
+  <input name="s" type="text" class="search-form-input" required>
+  <input class="search-form-submit" type="submit" value="">
   <div class="search-form-placeholder">
     <img class="search-form-placeholder-icon" src="http://dev-sixgill.pantheonsite.io/wp-content/themes/sixgill/images/search_icon.svg">
     Search...
