43b7d3801dc07784bf20512df51041cbe8163170
Daniil Leksin <danilaleksin@gmail.com>
2016-10-01 15:33:33 +0300

    [SIX-615] Fix card limit text for resource pages

--- wp-content/themes/sixgill/custom_build.js
+++ wp-content/themes/sixgill/custom_build.js
@@ -6329,17 +6329,16 @@ jQuery(function($) {
 });
 
 jQuery(function($) {
-  $('#wrapper').hide();
+	$('#wrapper').css('opacity', '1');
+	$('#loader-wrapper').hide();
+	$('#fp-nav').css('opacity', '1');
 
-    $(window).on("load", function() {
-        $('#wrapper').css('opacity', '1').show();
+	$(window).on("load", function() {
         // return back right dots for home page
         $('#fp-nav').css('opacity', '1');
-        $('#loader-wrapper').hide();
-	});
+    });
 
 });
-
 jQuery(function(){
 	if(window.screenType != 'desktop') return;
 	if(!$('.blog-post-wrapper').length) return;
