c65d56f8c996637777094a7a4b88c0dfc5118d0d
Michael Nazarov <mnazarov@WeezLabs.com>
2016-10-03 15:39:05 +0300

    added twitter meta to head

--- wp-content/themes/sixgill/share_scheme.php
+++ wp-content/themes/sixgill/share_scheme.php
@@ -1,3 +1,4 @@
+	<!-- FACEBOOK META TAGS -->
 	<?php
 		$shareImage = get_field('post_picture_to_share');
 	?>
@@ -10,3 +11,11 @@
 	<?php } ?>
 
  	<meta property="og:url" content="<?php echo wp_get_shortlink(); ?>" />
+ 	<!-- TWITTER META TAGS -->
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:site" content="@SixgillTech">
+	<meta name="twitter:title" content="<?php echo $post->post_title; ?>">
+	<meta name="twitter:description" content="<?php echo get_field('subtitle'); ?>">
+	<?php if ($shareImage) { ?>
+ 		<meta property="twitter:image" content="<?php echo $shareImage; ?>" />
+	<?php } ?>
