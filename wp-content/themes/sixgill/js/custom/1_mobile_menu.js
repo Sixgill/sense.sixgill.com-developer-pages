d7da3dc77761864677e24f613668446d4c503c3c
Michael Nazarov <mnazarov@WeezLabs.com>
2016-10-06 14:53:40 +0300

    refactored js code for menu

--- wp-content/themes/sixgill/js/custom/1_mobile_menu.js
+++ wp-content/themes/sixgill/js/custom/1_mobile_menu.js
@@ -1,13 +1,32 @@
 jQuery(function($) {
 	window.menuShowFlag = false;
 	var isMenuShown = false;
-	var savedScroll;
-	window.onScreenTypeChanged(function(newScreenTypeName) {
-		$('.primary-menu' ).removeAttr("style");
-		$('#menu-background' ).removeAttr("style");
-		$('.primary-menu' ).removeClass("stick-to-right");
-		$('#menu-background').removeClass("fill-opacity");
+	var primary_menu = $('.primary-menu');
+	var background = $('#menu-background');
+	var menu_trigger = $('#primary-menu-trigger');
+	var tablet_close = $('#close-menu-icon-tablet');
+	var body = $('body');
+	var attr = $(primary_menu).attr("style");
+
+	function resetStyles(cb){
+		console.log('RESET');
+		if(attr && attr!==false){
+			primary_menu.removeAttr("style");
+			background.removeAttr("style");
+		}
+		if(primary_menu.hasClass("stick-to-right")){
+			primary_menu.removeClass("stick-to-right");
+			background.removeClass("fill-opacity");
+		}
+		if(body.hasClass("overflow-hide")){
+			body.removeClass("overflow-hide");
+		}
 		isMenuShown = false;
+		if(cb){
+			cb();
+		}
+	}
+	function handleScreenSize(newScreenTypeName){
 		if(newScreenTypeName == "tablet"){
 			forTablet();
 		} else if(newScreenTypeName == "desktop"){
@@ -15,50 +34,75 @@ jQuery(function($) {
 		} else{
 			forMobile();
 		}
-	});
-	if(window.screenType == "tablet"){
-		forTablet();
-	} else if(window.screenType == "desktop"){
-		forDesktop();
-	} else{
-		forMobile();
 	}
+	function offListeners(tablet){
+		menu_trigger.off();
+		if(tablet){
+			tablet_close.off();
+		}
+	}
+	function hideBackground(notMobile){
+		if(!background.hasClass("hide")){
+			background.addClass("hide");
+		}
+		if (notMobile) {
+			if(primary_menu.hasClass("hide")){
+				primary_menu.removeClass("hide");
+			}
+		} else {
+			if(primary_menu.hasClass("hide")){
+				primary_menu.addClass("hide");
+			}
+			menu_trigger.removeClass("clicked");
+		}
+	}
+	window.onScreenTypeChanged(function(newScreenTypeName) {
+		resetStyles(function(){
+			handleScreenSize(newScreenTypeName);
+		});
+	});
+	handleScreenSize(window.screenType);
 	function forDesktop(){
-		$('#menu-background').addClass("hide");
-		$('.primary-menu' ).removeClass("hide");
+		console.log('DESKTOP');
+		hideBackground(true);
 	}
 	function forTablet(){
-		$('#primary-menu-trigger').off();
-		$('#close-menu-icon-tablet').off();
-		$('.primary-menu').removeClass("hide");
-		$('#primary-menu-trigger').click(function() {
-			$('.primary-menu' ).addClass("stick-to-right");
-			$('#menu-background').removeClass("hide");
-			$('#menu-background').addClass("fill-opacity");
+		console.log('TABLET');
+		offListeners(true);
+		hideBackground(true);
+		menu_trigger.click(function() {
+			primary_menu.addClass("stick-to-right");
+			background.removeClass("hide");
+			background.addClass("fill-opacity");
+			body.addClass("overflow-hide");
 		});
-		$('#close-menu-icon-tablet').click(function() {
-			$('.primary-menu' ).removeClass("stick-to-right");
-			$('#menu-background').removeClass("fill-opacity");
-			$('#menu-background').addClass("hide");
+		tablet_close.click(function() {
+			console.log("cannot happen right?!");
+			primary_menu.removeClass("stick-to-right");
+			background.removeClass("fill-opacity");
+			background.addClass("hide");
+			body.removeClass("overflow-hide");
 		});
 	}
 	function forMobile(){
-		$('#primary-menu-trigger').off();
-		$('#menu-background').addClass("hide");
-		$('.primary-menu').addClass("hide");
-		$('.primary-menu-trigger-mobile').removeClass("clicked");
-		$('#primary-menu-trigger').click(function() {
+		console.log('MOBILE');
+		offListeners(false);
+		hideBackground(false);
+		menu_trigger.click(function() {
 			if(isMenuShown) {
-				$( '.primary-menu' ).fadeTo(500, 0,function(){
-					$('.primary-menu' ).addClass("hide");
+				console.log("cannot happen right?!");
+				primary_menu.fadeTo(500, 0,function(){
+					primary_menu.addClass("hide");
+					body.removeClass("overflow-hide");
 					isMenuShown = false;
 				});
 			} else {
-				$('.primary-menu' ).removeClass("hide");
-				$( '.primary-menu' ).fadeTo(500, 1);
+				primary_menu.removeClass("hide");
+				primary_menu.fadeTo(500, 1);
+				body.addClass("overflow-hide");
 				isMenuShown = true;
 			}
-			$('.primary-menu-trigger-mobile').toggleClass("clicked");
+			menu_trigger.toggleClass("clicked");
 			window.menuShowFlag = isMenuShown;
 		});
 	}
