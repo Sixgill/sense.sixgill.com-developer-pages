a59fdf4c0b02d5a6751ec481efd7a248fc1b4b69
Daniil Leksin <danilaleksin@gmail.com>
2016-10-07 15:48:48 +0300

    Add gulp collector for less files

---
+++ sign-up/gulpfile.js
@@ -0,0 +1,20 @@
+// How to isntall npm&gulp use link below:
+// https://www.sitepoint.com/introduction-gulp-js/
+
+//Before start gulp, pls install node_modules via the command below:
+//npm install
+
+// include gulp
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	less = require('gulp-less');
+
+// create build.css from ./less dir
+// To run gulp type: gulp
+
+gulp.task('default', function () {
+	gulp.src('less/*.less')
+		.pipe(less())
+		.pipe(concat('signup_build.css'))
+		.pipe(gulp.dest(''));
+});
