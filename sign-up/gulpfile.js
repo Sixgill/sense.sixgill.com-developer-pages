fc996f8ca79cb5ddd5de04bcb21be430f991dea4
vterekhov <haosx86@gmail.com>
2016-10-10 17:21:46 +0300

    refactoring

--- sign-up/gulpfile.js
+++ sign-up/gulpfile.js
@@ -14,8 +14,9 @@ var gulp = require('gulp'),
 
 // To run gulp type: gulp
 
-var htmlHead = '<!DOCTYPE html>\n<html dir="ltr" lang="en-US">\n<head>\n	<meta http-equiv="content-type" content="text/html; charset=utf-8" />\n	<link rel="stylesheet" type="text/css" href="css/signup_build.css">\n	<link href="https://fonts.googleapis.com/css?family=Lato:400,700,900,300,600" rel="stylesheet" type="text/css">\n</head>\n<body>\n'
-var htmlTail = "\n</body>\n</html>"
+//TODO: redo it
+var htmlHead = '<!DOCTYPE html>\n<html dir="ltr" lang="en-US">\n<head>\n	<meta http-equiv="content-type" content="text/html; charset=utf-8" />\n	<link rel="stylesheet" type="text/css" href="signup_build.css">\n	<link href="https://fonts.googleapis.com/css?family=Lato:400,700,900,300,600" rel="stylesheet" type="text/css">\n</head>\n<body>\n';
+var htmlTail = "\n</body>\n</html>";
 
 gulp.task('default',
   (callback) => runSequence(
@@ -27,16 +28,16 @@ gulp.task('default',
 );
 
 // signup_build.css from ./less dir
-gulp.task('less', 
+gulp.task('less',
 	() => gulp.src('less/*.less')
 		.pipe(less())
-		.pipe(concat('css/signup_build.css'))
+		.pipe(concat('./signup_build.css'))
 		.pipe(gulp.dest(''))
 );
 
 // put html sources into /html_src dir
-gulp.task('html', 
-	() => gulp.src(['./new_pass_confirm.html', './pass_change.html'])
+gulp.task('html',
+	() => gulp.src(['./src/new_pass_confirm.html', './src/pass_change.html'])
 		.pipe(insert.prepend(htmlHead))
 		.pipe(insert.append(htmlTail))
 		.pipe(gulp.dest('./src'))
@@ -44,16 +45,16 @@ gulp.task('html',
 
 
 gulp.task('inject',
-	() => gulp.src(['./*.html', '!./footer.html', '!./header.html', '!./new_pass_confirm.html', '!./pass_change.html'])
+	() => gulp.src(['./src/*.html', '!./src/footer.html', '!./src/header.html', '!./src/new_pass_confirm.html', '!./src/pass_change.html'])
 	// Insert HEADER
-	.pipe(inject(gulp.src(['./header.html']), {
+	.pipe(inject(gulp.src(['./src/header.html']), {
 		starttag: '<!-- inject:header:{{ext}} -->',
 		transform: function (filePath, file) {
       		return file.contents.toString('utf8')
 			}
 	}))
 	// Insert FOOTERS
-	.pipe(inject(gulp.src(['./footer.html']), {
+	.pipe(inject(gulp.src(['./src/footer.html']), {
 		starttag: '<!-- inject:footer:{{ext}} -->',
 		transform: function (filePath, file) {
       		return file.contents.toString('utf8')
@@ -62,5 +63,5 @@ gulp.task('inject',
 	// Insert head and tail
 	.pipe(insert.prepend(htmlHead))
 	.pipe(insert.append(htmlTail))
-	.pipe(gulp.dest('./src'))
+	.pipe(gulp.dest('.'))
 );
